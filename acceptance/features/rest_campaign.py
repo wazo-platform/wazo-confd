cb8d5b36c1b50f8dbd2a375cb82ebcaeb4d35696
Jirka <jhlavacek@avencall.com>
2012-10-24 18:49:41 +0200

    JSON encoder rework

--- acceptance/features/rest_campaign.py
+++ acceptance/features/rest_campaign.py
@@ -1,18 +1,19 @@
 
 
 from gevent import httplib
-from tests.test_config import TestConfig
-from xivo_cti_encoding_interface.cti_encoder_provider import CtiEncoderProvider
+
+from recording_config import RecordingConfig
+from cti_protocol import CtiProtocol
 
 
 class RestCampaign(object):
 
     def create(self, campaign_name):
-        connection = httplib.HTTPConnection(TestConfig.XIVO_RECORD_SERVICE_ADDRESS + ":" + str(TestConfig.XIVO_RECORD_SERVICE_PORT))
-        requestURI = TestConfig.XIVO_REST_SERVICE_ROOT_PATH + TestConfig.XIVO_RECORDING_SERVICE_PATH + "/" + campaign_name
+        connection = httplib.HTTPConnection(RecordingConfig.XIVO_RECORD_SERVICE_ADDRESS + ":" + str(RecordingConfig.XIVO_RECORD_SERVICE_PORT))
+        requestURI = RecordingConfig.XIVO_REST_SERVICE_ROOT_PATH + RecordingConfig.XIVO_RECORDING_SERVICE_PATH + "/" + campaign_name
         body_content = """{"test":{"id": "6", "name": "jirka"}}"""
-        body = CtiEncoderProvider.encoder.encode(body_content)
-        headers = TestConfig.CTI_REST_DEFAULT_CONTENT_TYPE
+        body = CtiProtocol.encode(body_content)
+        headers = RecordingConfig.CTI_REST_DEFAULT_CONTENT_TYPE
         print("body: " + body)
 
         connection.request("POST", requestURI, body, headers)
@@ -21,6 +22,8 @@ class RestCampaign(object):
         print("reply: " + reply.read())
         replyHeader = reply.getheaders()
         print("replyHeader: " + str(replyHeader))
+
+
         #assert rHeader ==
 
         return False
