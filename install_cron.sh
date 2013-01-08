541f6b471c3e066cb72c86b39817b1a444c95f75
Jean Aunis <jaunis@avencall.com>
2013-01-08 18:35:32 +0100

    modified frequency and script location for crontab

--- install_cron.sh
+++ install_cron.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-test -x /etc/cron.minutely || mkdir /etc/cron.minutely
-cp delete_old_items /etc/cron.minutely/delete_old_items
-chmod u+x /etc/cron.minutely/delete_old_items
+cp delete_old_items /usr/local/bin/delete_old_items
+chmod u+x /usr/local/bin/delete_old_items
 TMP_CRONTAB=/tmp/crontab
 crontab -l > $TMP_CRONTAB.txt
-echo "* * * * *  cd / && run-parts --report /etc/cron.minutely" >> $TMP_CRONTAB.txt
+echo "0 0 * * *  /usr/local/bin/delete_old_items" >> $TMP_CRONTAB.txt
 crontab $TMP_CRONTAB.txt
 RES=$?
 rm ${TMP_CRONTAB}.txt
