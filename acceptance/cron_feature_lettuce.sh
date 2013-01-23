b12db52b7e48fa401d7d13e67a94b0bc9655647e
Jean Aunis <jaunis@avencall.com>
2013-01-23 14:54:21 +0100

    added xunit report to lettuce tests

--- acceptance/cron_feature_lettuce.sh
+++ acceptance/cron_feature_lettuce.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-PYTHONPATH=..:../xivo_recording:../../xivo-dao/xivo-dao lettuce features/cron.feature
+PYTHONPATH=..:../xivo_recording:../../xivo-dao/xivo-dao lettuce features/cron.feature --with-xunit --verbosity=3 --xunit-file=xunit-tests-cron.xml
