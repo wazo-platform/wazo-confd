15bffed6e29e11d26e950338d52b46e7802f0ce6
Jirka Hlavacek <jhlavacek@avencall.com>
2013-01-28 12:43:33 +0100

    Fix path in acceptance launchers

--- xivo-restapi/acceptance/campaigns_feature_lettuce.sh
+++ xivo-restapi/acceptance/campaigns_feature_lettuce.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-PYTHONPATH=..:../xivo_restapi:../../xivo-dao/xivo-dao:../../xivo-acceptance:../../xivo-ws lettuce features/campaigns.feature --with-xunit --verbosity=3 --xunit-file=xunit-tests-campaigns.xml
+PYTHONPATH=..:../xivo_restapi:../../../xivo-dao/xivo-dao:../../../xivo-acceptance:../../../xivo-ws lettuce features/campaigns.feature --with-xunit --verbosity=3 --xunit-file=xunit-tests-campaigns.xml
