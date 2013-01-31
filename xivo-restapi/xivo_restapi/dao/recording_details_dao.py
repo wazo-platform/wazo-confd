477afc9b50b47f777af983a9c3a34f4f303c717b
Jirka Hlavacek <jhlavacek@avencall.com>
2013-01-31 15:14:42 +0100

    Fix following evolution of DAO

--- xivo-restapi/xivo_restapi/dao/recording_details_dao.py
+++ xivo-restapi/xivo_restapi/dao/recording_details_dao.py
@@ -27,7 +27,7 @@ from xivo_restapi.dao.generic_dao import GenericDao
 from xivo_restapi.dao.helpers.query_utils import get_all_data, \
     get_paginated_data
 from xivo_dao.helpers.db_manager import daosession_class
-from xivo_dao.helpers import db_manager
+from xivo_dao.helpers import config
 import logging
 
 logger = logging.getLogger(__name__)
@@ -43,7 +43,7 @@ class RecordingDetailsDbBinder(object):
     __tablename__ = "recording"
 
     def __init__(self):
-        self._new_from_uri(db_manager._DB_URI)
+        self._new_from_uri(config.DB_URI)
 
     @daosession_class
     def get_recordings_as_list(self, session, campaign_id, search=None, pagination=None):
