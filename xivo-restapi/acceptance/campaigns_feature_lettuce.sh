2b63cae070e45e8faca7c7c313b90b1040935846
Jean Aunis <jaunis@avencall.com>
2013-04-08 13:36:58 +0200

    fixed PYTHONPATH in shell scripts

--- xivo-restapi/acceptance/campaigns_feature_lettuce.sh
+++ xivo-restapi/acceptance/campaigns_feature_lettuce.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-PYTHONPATH=..:../xivo_restapi:../../../xivo-dao/xivo-dao lettuce features/campaigns.feature --with-xunit --verbosity=3 --xunit-file=xunit-tests-campaigns.xml
+PYTHONPATH=..:../xivo_restapi:../../../xivo-dao/xivo-dao:../../../xivo-lib-python/xivo-lib-python lettuce features/campaigns.feature --with-xunit --verbosity=3 --xunit-file=xunit-tests-campaigns.xml
--- xivo-restapi/acceptance/voicemails_feature_lettuce.sh
+++ xivo-restapi/acceptance/voicemails_feature_lettuce.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-PYTHONPATH=..:../../xivo_restapi:../../../xivo-dao/xivo-dao lettuce features/voicemails.feature --with-xunit --verbosity=3 --xunit-file=xunit-tests-agent.xml
+PYTHONPATH=..:../../xivo_restapi:../../../xivo-dao/xivo-dao:../../../xivo-lib-python/xivo-lib-python lettuce features/voicemails.feature --with-xunit --verbosity=3 --xunit-file=xunit-tests-agent.xml
