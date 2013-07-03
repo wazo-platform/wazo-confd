9e557e1e323c291a0f3a7f4e6381326d0173740b
Cedric Abunar <cabunar@avencall.com>
2013-07-03 13:40:53 -0400

    remove unused files

--- xivo-restapi/acceptance/users_feature_lettuce.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-PYTHONPATH=..:../../xivo_restapi:../../../xivo-dao/xivo-dao:../../../xivo-lib-python/xivo-lib-python:../../../xivo-acceptance lettuce features/users.feature --with-xunit --verbosity=3 --xunit-file=xunit-tests-user.xml
