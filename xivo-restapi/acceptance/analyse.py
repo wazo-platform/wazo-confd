14394eac6a755aa00d2ab0d6dd1a19127d042eb5
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2013-08-05 10:10:40 -0400

    add some tests, make xivo_host an argument and some cleanup

--- xivo-restapi/acceptance/analyse.py
+++ xivo-restapi/acceptance/analyse.py
@@ -4,9 +4,8 @@ import sys
 import traceback
 from httplib2 import Http
 from json import dumps, loads
-import inspect
+import argparse
 
-URI_BASE = "https://192.168.32.186:50051/1.1"
 FAIL_COLOR = '\033[91m'
 END_COLOR = '\033[0m'
 HEADER_COLOR = '\033[95m'
@@ -20,6 +19,9 @@ headers = {'Content-type': 'application/json'}
 def handle_test(function):
     def handleProblems():
         try:
+            caller_name = function.__name__
+            splitter = '-' * 120
+            print ("%s%s\n%s\n%s%s\n" % (HEADER_COLOR, splitter, caller_name, splitter, END_COLOR))
             function()
 
         except AssertionError:
@@ -30,10 +32,6 @@ def handle_test(function):
             splitter = len(failed_message) * '='
             print("%s\n%s\n%s\n%s" % (FAIL_COLOR, failed_message, splitter, END_COLOR))
             handle_test.failed_tests += 1
-        #else:
-        #    message = "Test passed!"
-        #    splitter = len(message) * '='
-        #    print ("\n%s%s\n%s\n%s" % (PASS_COLOR, message, splitter, END_COLOR))
 
     return handleProblems
 
@@ -155,24 +153,116 @@ def test_create_link_missing_lineid():
 @handle_test
 def test_create_link_unexisting_extension():
     data = dict(firstname="toto")
-    resp, content = h.request(URI_BASE + "/users/", "POST", headers=headers,  body=dumps(data))
-    new_user_id = loads(content)['id']
+    resp_p1, content_p1 = h.request(URI_BASE + "/users/", "POST", headers=headers,  body=dumps(data))
+    new_user_id = loads(content_p1)['id']
+    _print_result(resp_p1, content_p1)
 
     data = dict(context="default")
-    resp, content = h.request(URI_BASE + "/lines_sip/", "POST", headers=headers,  body=dumps(data))
-    new_line_id = loads(content)['id']
+    resp_p2, content_p2 = h.request(URI_BASE + "/lines_sip/", "POST", headers=headers,  body=dumps(data))
+    new_line_id = loads(content_p2)['id']
+    _print_result(resp_p2, content_p2)
 
     data = dict(user_id=new_user_id, line_id=new_line_id, extension_id="999999999")
     resp, content = h.request(URI_BASE + "/user_links/", "POST", headers=headers,  body=dumps(data))
     _print_result(resp, content)
 
+    _delete_user(new_user_id)
+    _delete_line(new_line_id)
+
     assert(resp['status'] == '400')
 
 
+@handle_test
+def test_create_link_unexisting_line():
+    data = dict(firstname="Greg")
+    resp, content = h.request(URI_BASE + "/users/", "POST", headers=headers,  body=dumps(data))
+    new_user_id = loads(content)['id']
+    _print_result(resp, content)
+
+    data = dict(exten="1000", context="default")
+    resp, content = h.request(URI_BASE + "/extensions/", "POST", headers=headers,  body=dumps(data))
+    new_extension_id = loads(content)['id']
+    _print_result(resp, content)
+
+    data = dict(user_id=new_user_id, line_id="999999999", extension_id=new_extension_id)
+    resp, content = h.request(URI_BASE + "/user_links/", "POST", headers=headers,  body=dumps(data))
+    _print_result(resp, content)
+
+    _delete_user(new_user_id)
+    _delete_extension(new_extension_id)
+
+    assert(resp['status'] == '400')
+
+
+@handle_test
+def test_create_link_unexisting_user():
+    assert("Not implemented" is True)
+
+
+@handle_test
+def test_create_link_default_context():
+    data = dict(firstname="Greg")
+    resp, content = h.request(URI_BASE + "/users/", "POST", headers=headers,  body=dumps(data))
+    new_user_id = loads(content)['id']
+    _print_result(resp, content)
+
+    data = dict(context="default")
+    resp, content = h.request(URI_BASE + "/lines_sip/", "POST", headers=headers,  body=dumps(data))
+    new_line_id = loads(content)['id']
+    _print_result(resp, content)
+
+    data = dict(exten="1000", context="default")
+    resp, content = h.request(URI_BASE + "/extensions/", "POST", headers=headers,  body=dumps(data))
+    new_extension_id = loads(content)['id']
+    _print_result(resp, content)
+
+    data = dict(user_id=new_user_id, line_id=new_line_id, extension_id=new_extension_id)
+    resp, content = h.request(URI_BASE + "/user_links/", "POST", headers=headers,  body=dumps(data))
+    new_link_id = loads(content)['id']
+    _print_result(resp, content)
+
+    _delete_link(new_link_id)
+    _delete_user(new_user_id)
+    _delete_line(new_line_id)
+    _delete_extension(new_extension_id)
+
+    assert(resp['status'] == '201')
+    assert(new_link_id)
+
+
+@handle_test
+def test_create_link_other_context():
+    data = dict(firstname="Greg")
+    resp, content = h.request(URI_BASE + "/users/", "POST", headers=headers,  body=dumps(data))
+    new_user_id = loads(content)['id']
+    _print_result(resp, content)
+
+    data = dict(context="statscenter")
+    resp, content = h.request(URI_BASE + "/lines_sip/", "POST", headers=headers,  body=dumps(data))
+    new_line_id = loads(content)['id']
+    _print_result(resp, content)
+
+    data = dict(exten="1000", context="default")
+    resp, content = h.request(URI_BASE + "/extensions/", "POST", headers=headers,  body=dumps(data))
+    new_extension_id = loads(content)['id']
+    _print_result(resp, content)
+
+    data = dict(user_id=new_user_id, line_id=new_line_id, extension_id=new_extension_id)
+    resp, content = h.request(URI_BASE + "/user_links/", "POST", headers=headers,  body=dumps(data))
+    new_link_id = loads(content)['id']
+    _print_result(resp, content)
+
+    _delete_link(new_link_id)
+    _delete_user(new_user_id)
+    _delete_line(new_line_id)
+    _delete_extension(new_extension_id)
+
+    assert(resp['status'] == '201')
+    assert(new_link_id)
+
+
 def _print_result(resp, content):
-    caller_name = inspect.stack()[1][3]
-    splitter = '-' * 120
-    print ("%s%s\n%s\n%s%s\n%s\n%s" % (HEADER_COLOR, splitter, caller_name, splitter, END_COLOR, resp, content))
+    print ("%s\n%s\n" % (resp, content))
 
 
 def _print_failed_tests(count):
@@ -181,7 +271,29 @@ def _print_failed_tests(count):
     print ("%s\n%s\n%s\n%s%s" % (FAIL_COLOR, splitter, failed_message, splitter, END_COLOR))
 
 
+def _delete_link(link_id):
+    resp, content = h.request("%s%s%s" % (URI_BASE, "/user_links/", link_id), "DELETE", headers=headers)
+
+
+def _delete_user(user_id):
+    resp, content = h.request("%s%s%s" % (URI_BASE, "/users/", user_id), "DELETE", headers=headers)
+
+
+def _delete_line(line_id):
+    resp, content = h.request("%s%s%s" % (URI_BASE, "/lines_sip/", line_id), "DELETE", headers=headers)
+
+
+def _delete_extension(extension_id):
+    resp, content = h.request("%s%s%s" % (URI_BASE, "/extensions/", extension_id), "DELETE", headers=headers)
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser('Analyse Rest API acceptance tests')
+    parser.add_argument('xivo_host', help='IP or domain of target XiVO installation')
+
+    args = parser.parse_args()
+    URI_BASE = "https://%s:50051/1.1" % (args.xivo_host)
+
     handle_test.failed_tests = 0
     print "Running Acceptance tests on REST API"
 
@@ -198,6 +310,10 @@ if __name__ == "__main__":
     test_create_link_invalid_values()
     test_create_link_invalid_params()
     test_create_link_missing_lineid()
-    #test_create_link_unexisting_extension()
+    test_create_link_unexisting_extension()
+    test_create_link_unexisting_line()
+    test_create_link_unexisting_user()
+    test_create_link_default_context()
+    test_create_link_other_context()
 
     _print_failed_tests(handle_test.failed_tests)
