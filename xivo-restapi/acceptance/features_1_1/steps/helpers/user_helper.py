a6f2c485292de8a07230af91764d61d301f7936a
Gregory Eric Sanderson <gsanderson@avencall.com>
2013-08-07 16:16:19 -0400

    use remote execution for preconditions

--- xivo-restapi/acceptance/features_1_1/steps/helpers/user_helper.py
+++ xivo-restapi/acceptance/features_1_1/steps/helpers/user_helper.py
@@ -1,26 +1,35 @@
+from helpers.remote import remote_exec
+
 from xivo_dao.data_handler.user import dao as user_dao
-from xivo_dao.data_handler.user.model import User
 
 
 def delete_all():
-    for user in user_dao.find_all():
-        user_dao.delete(user)
+    remote_exec(_delete_all)
+
+
+def _delete_all(channel):
+    from xivo_dao.data_handler.user import services as user_services
+    from xivo_dao.data_handler.user_line_extension import services as ule_services
+
+    for user in user_services.find_all():
+
+        ules = ule_services.find_all_by_user_id(user.id)
+        for ule in ules:
+            ule_services.delete(ule)
+
+        user_services.delete(user)
 
 
 def create_user(userinfo):
-    _delete_user_if_needed(userinfo)
-    _create_user(userinfo)
+    remote_exec(_create_user, userinfo=userinfo)
 
 
-def _delete_user_if_needed(userinfo):
-    user = user_dao.find_user(userinfo['firstname'], userinfo['lastname'])
-    if user:
-        user_dao.delete(user)
+def _create_user(channel, userinfo):
+    from xivo_dao.data_handler.user import services as user_services
+    from xivo_dao.data_handler.user.model import User
 
-
-def _create_user(userinfo):
     user = User(**userinfo)
-    user_dao.create(user)
+    user_services.create(user)
 
 
 def find_user_by_name(name):
