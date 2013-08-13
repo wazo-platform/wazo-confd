b809d9c2d3aa096d71a64dab37ade1c3d3cf2df5
Cedric Abunar <cabunar@avencall.com>
2013-08-13 15:40:55 -0400

    clean and remove duplicate steps

--- xivo-restapi/acceptance/features_1_1/steps/extensions_steps.py
+++ xivo-restapi/acceptance/features_1_1/steps/extensions_steps.py
@@ -28,7 +28,8 @@ def given_i_have_no_extensions(step):
 @step(u'Given I only have the following extensions:')
 def given_i_have_the_following_extensions(step):
     extension_helper.delete_all()
-    extension_helper.create_extensions(step.hashes)
+    for exteninfo in step.hashes:
+        extension_helper.create_extensions([exteninfo])
 
 
 @step(u'When I access the list of extensions')
--- xivo-restapi/acceptance/features_1_1/steps/user_link_steps.py
+++ xivo-restapi/acceptance/features_1_1/steps/user_link_steps.py
@@ -15,10 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from helpers import user_link_ws, user_helper, line_sip_helper, extension_helper
-from lettuce import step, world
-
 from hamcrest import *
+from helpers import user_link_ws
+from lettuce import step, world
 
 
 @step(u'When I create an empty link')
@@ -39,29 +38,8 @@ def when_i_create_a_link_with_the_following_invalid_parameters(step):
     world.response = user_link_ws.create_user_link(parameters)
 
 
-@step(u'Given I only have the following users:')
-def given_i_created_the_following_users(step):
-    user_helper.delete_all()
-    for userinfo in step.hashes:
-        user_helper.create_user(userinfo)
-
-
-@step(u'Given I only have the following lines:')
-def given_i_created_the_following_lines(step):
-    line_sip_helper.delete_all()
-    for lineinfo in step.hashes:
-        line_sip_helper.create_line_sip(lineinfo)
-
-
-@step(u'Given I only have the following extensions:')
-def given_i_have_the_following_extensions(step):
-    extension_helper.delete_all()
-    for exteninfo in step.hashes:
-        extension_helper.create_extensions([exteninfo])
-
-
-@step(u'Then I get the user_links with the following properties:')
-def then_i_get_the_lines_with_the_following_properties(step):
+@step(u'Then I get the user_links with the following parameters:')
+def then_i_get_the_lines_with_the_following_parameters(step):
     for expected_data in step.hashes:
         assert_that(world.response.data['items'], has_item(
             has_entries(_extract_parameters(expected_data))
