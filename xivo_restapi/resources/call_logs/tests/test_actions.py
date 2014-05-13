7fcda6fbf351fa60bcab6b01e0cf7f31c5d279df
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-05-13 15:10:34 -0400

    add missing unit tests on call logs resource

--- xivo_restapi/resources/call_logs/tests/test_actions.py
+++ xivo_restapi/resources/call_logs/tests/test_actions.py
@@ -85,3 +85,12 @@ class TestCallLogActions(TestResources):
         assert_that(result.headers, has_entries({
             'Content-disposition': 'attachment;filename=xivo-call-logs.csv',
         }))
+
+    def test_list_call_logs_with_wrongly_formatted_date(self):
+        expected_status_code = 400
+        expected_message = '["Invalid parameters: start_date"]'
+
+        result = self.app.get(BASE_URL + '?start_date=20AB-12-14')
+
+        assert_that(result.status_code, equal_to(expected_status_code))
+        assert_that(result.data, equal_to(expected_message))
