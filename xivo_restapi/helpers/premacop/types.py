ff46cbdd697b323cf4b2f6936f6b76f176d74fa4
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-07-14 15:41:06 -0400

    add validators for checking the type of a field

---
+++ xivo_restapi/helpers/premacop/types.py
@@ -0,0 +1,67 @@
+import collections
+
+from errors import ValidationError
+
+
+class FieldType(object):
+
+    error_message = "'{value}' is not valid"
+    type_class = None
+
+    def validate(self, value):
+        if value is None:
+            return
+        if not isinstance(value, self.type_class):
+            self.raise_error(value)
+
+    def raise_error(self, value):
+        msg = self.error_message.format(value=value)
+        raise ValidationError(msg)
+
+
+class Int(FieldType):
+
+    type_class = int
+    error_message = "'{value}' is not a numeric integer"
+
+    def validate(self, value):
+        if isinstance(value, bool):
+            self.raise_error(value)
+        super(Int, self).validate(value)
+
+
+class Boolean(FieldType):
+
+    type_class = bool
+    error_message = "'{value}' is not a boolean"
+
+
+class Unicode(FieldType):
+
+    type_class = unicode
+    error_message = "'{value}' is not a unicode string"
+
+
+class Float(FieldType):
+
+    type_class = float
+    error_message = "'{value}' is not a numeric float"
+
+
+class Array(FieldType):
+
+    error_message = "'{value}' is not an array-like sequence'"
+
+    def __init__(self, field_type, *validators):
+        self.field_type = field_type
+        self.validators = validators
+
+    def validate(self, value):
+        if value is None:
+            return
+        if not isinstance(value, collections.Iterable):
+            self.raise_error(value)
+        for element in value:
+            self.field_type.validate(element)
+            for validator in self.validators:
+                validator(element)
