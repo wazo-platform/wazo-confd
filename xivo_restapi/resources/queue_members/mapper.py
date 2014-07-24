cdb8c701536be5af6e8b43dd342e4f8e2df05907
Jean Aunis <jaunis@avencall.com>
2014-07-24 13:48:42 +0200

    created the mapper for queue_members service

---
+++ xivo_restapi/resources/queue_members/mapper.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+# mapping = {model_field: api_field}
+MAPPING = {
+    'queue_id': 'queue_id',
+    'agent_id': 'agent_id',
+    'penalty': 'penalty'
+    }
+
+
+def add_links_to_dict(result_dict, queue_member):
+    pass
