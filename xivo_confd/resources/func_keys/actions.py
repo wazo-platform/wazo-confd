51abc5a7653607fc7784514065aee263ceb0445a
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-11-11 15:27:05 -0500

    refactor func key service to use converter

--- xivo_confd/resources/func_keys/actions.py
+++ xivo_confd/resources/func_keys/actions.py
@@ -2,26 +2,36 @@ import logging
 
 from flask import request, make_response
 
-from xivo_confd.helpers.formatter import Formatter
-from xivo_confd.resources.func_keys import mapper
-from xivo_confd.helpers import serializer
 from xivo_confd.helpers.common import extract_search_parameters
 from xivo_dao.data_handler.func_key.model import FuncKey
 from xivo_dao.data_handler.func_key import services as func_key_services
 
+from xivo_confd.flask_http_server import content_parser
+from xivo_confd.helpers.mooltiparse import Field, Int, Unicode
+
+from xivo_confd.helpers.converter import Converter
+
 
 logger = logging.getLogger(__name__)
-formatter = Formatter(mapper, serializer, FuncKey)
+
+document = content_parser.document(
+    Field('id', Int()),
+    Field('type', Unicode()),
+    Field('destination', Unicode()),
+    Field('destination_id', Unicode()),
+)
+
+converter = Converter.for_document(document, FuncKey)
 
 
 def list():
     search_parameters = extract_search_parameters(request.args)
     search_result = func_key_services.search(**search_parameters)
-    result = formatter.list_to_api(search_result.items, search_result.total)
-    return make_response(result, 200)
+    items = converter.encode_list(search_result.items, search_result.total)
+    return make_response(items, 200)
 
 
 def get(func_key_id):
     func_key = func_key_services.get(func_key_id)
-    result = formatter.to_api(func_key)
-    return make_response(result, 200)
+    encoded_func_key = converter.encode(func_key)
+    return make_response(encoded_func_key, 200)
--- xivo_confd/resources/func_keys/tests/test_actions.py
+++ xivo_confd/resources/func_keys/tests/test_actions.py
@@ -26,35 +26,35 @@ from hamcrest import assert_that, equal_to
 class TestFuncKeyActions(unittest.TestCase):
 
     @patch('xivo_confd.resources.func_keys.actions.make_response')
-    @patch('xivo_confd.resources.func_keys.actions.formatter.list_to_api')
+    @patch('xivo_confd.resources.func_keys.actions.converter.encode_list')
     @patch('xivo_dao.data_handler.func_key.services.search')
     @patch('xivo_confd.resources.func_keys.actions.request')
-    def test_list(self, request, func_key_search, list_to_api, make_response):
+    def test_list(self, request, func_key_search, encode_list, make_response):
         request.args = {'limit': '1'}
         search_result = func_key_search.return_value = Mock(SearchResult)
-        formatted_list = list_to_api.return_value = Mock()
+        formatted_list = encode_list.return_value = Mock()
         response = make_response.return_value = Mock()
 
         result = actions.list()
 
         func_key_search.assert_called_once_with(limit=1)
-        list_to_api.assert_called_once_with(search_result.items, search_result.total)
+        encode_list.assert_called_once_with(search_result.items, search_result.total)
         make_response.assert_called_once_with(formatted_list, 200)
         assert_that(result, equal_to(response))
 
     @patch('xivo_confd.resources.func_keys.actions.make_response')
-    @patch('xivo_confd.resources.func_keys.actions.formatter.to_api')
+    @patch('xivo_confd.resources.func_keys.actions.converter.encode')
     @patch('xivo_dao.data_handler.func_key.services.get')
-    def test_get(self, func_key_get, formatter_to_api, make_response):
+    def test_get(self, func_key_get, converter_encode, make_response):
         func_key_id = 1
 
         func_key = func_key_get.return_value = Mock(FuncKey)
-        formatted_func_key = formatter_to_api.return_value = Mock()
+        formatted_func_key = converter_encode.return_value = Mock()
         response = make_response.return_value = Mock()
 
         result = actions.get(func_key_id)
 
         func_key_get.assert_called_once_with(func_key_id)
-        formatter_to_api.assert_called_once_with(func_key)
+        converter_encode.assert_called_once_with(func_key)
         make_response.assert_called_once_with(formatted_func_key, 200)
         assert_that(result, equal_to(response))
