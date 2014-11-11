2f3fa13f8b23fac0e195924f84b8774f647381be
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-11-11 14:47:19 -0500

    refactor cti_profile resource to use converter

--- xivo_confd/resources/cti_profiles/actions.py
+++ xivo_confd/resources/cti_profiles/actions.py
@@ -19,27 +19,36 @@ from flask.blueprints import Blueprint
 
 from xivo_confd import config
 from xivo_confd.helpers.route_generator import RouteGenerator
-from xivo_confd.helpers import serializer
 from flask.helpers import make_response
 from xivo_dao.data_handler.cti_profile import services
-from xivo_confd.helpers.formatter import Formatter
 from xivo_dao.data_handler.cti_profile.model import CtiProfile
-from . import mapper
+
+from xivo_confd.helpers.converter import Converter
+from xivo_confd.flask_http_server import content_parser
+from xivo_confd.helpers.mooltiparse import Field, Unicode, Int
+
 
 blueprint = Blueprint('cti_profiles', __name__, url_prefix='/%s/cti_profiles' % config.VERSION_1_1)
 route = RouteGenerator(blueprint)
-formatter = Formatter(mapper, serializer, CtiProfile)
+
+
+document = content_parser.document(
+    Field('id', Int()),
+    Field('name', Unicode())
+)
+
+converter = Converter.for_document(document, CtiProfile, 'cti_profiles')
 
 
 @route('', methods=['GET'])
 def find_all():
     profiles = services.find_all()
-    result = formatter.list_to_api(profiles)
-    return make_response(result, 200)
+    items = converter.encode_list(profiles)
+    return make_response(items, 200)
 
 
-@route('/<int:cti_profile_id>', methods=['GET'])
-def get(cti_profile_id):
-    profile = services.get(cti_profile_id)
-    result = formatter.to_api(profile)
+@route('/<int:resource_id>', methods=['GET'])
+def get(resource_id):
+    profile = services.get(resource_id)
+    result = converter.encode(profile)
     return make_response(result, 200)
