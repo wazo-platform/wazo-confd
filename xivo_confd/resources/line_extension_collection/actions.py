dc76e54d8273878083e5be05f73645a581d4b88b
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-11-12 14:36:12 -0500

    refactor line_extension_collection to use converter

--- xivo_confd/resources/line_extension_collection/actions.py
+++ xivo_confd/resources/line_extension_collection/actions.py
@@ -16,30 +16,49 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+from flask import request, url_for, make_response
+
+from xivo_confd.resources.lines.routes import line_route
+
+from xivo_dao.data_handler.line_extension.model import LineExtension
 from xivo_dao.data_handler.line_extension import services as line_extension_services
 
 from xivo_confd.helpers import url
-from xivo_confd.resources.line_extension_collection.formatter import LineExtensionFormatter
+from xivo_confd.flask_http_server import content_parser
+from xivo_confd.helpers.mooltiparse import Field, Int
+from xivo_confd.helpers.converter import Converter
 
-formatter = LineExtensionFormatter()
+document = content_parser.document(
+    Field('line_id', Int()),
+    Field('extension_id', Int())
+)
 
-
-def associate_extension(line_id, parameters):
-    url.check_line_exists(line_id)
-    model = formatter.dict_to_model(line_id, parameters)
-    created_model = line_extension_services.associate(model)
-    return formatter.to_api(created_model)
-
-
-def dissociate_extension(line_id, extension_id):
-    url.check_line_exists(line_id)
-    url.check_extension_exists(extension_id)
-    model = formatter.model_from_ids(line_id, extension_id)
-    line_extension_services.dissociate(model)
-    return ''
+converter = Converter.for_association(document, LineExtension, {'lines': 'line_id',
+                                                                'extensions': 'extension_id'})
 
 
+@line_route('/<int:line_id>/extensions')
 def list_extensions(line_id):
     url.check_line_exists(line_id)
     line_extensions = line_extension_services.get_all_by_line_id(line_id)
-    return formatter.list_to_api(line_extensions)
+    items = converter.encode_list(line_extensions)
+    return make_response(items, 200)
+
+
+@line_route('/<int:line_id>/extensions', methods=['POST'])
+def associate_line_extension(line_id):
+    url.check_line_exists(line_id)
+    model = converter.decode(request)
+    created_model = line_extension_services.associate(model)
+    encoded_model = converter.encode(created_model)
+    location = url_for('.list_extensions', line_id=line_id)
+    return make_response(encoded_model, 201, {'Location': location})
+
+
+@line_route('/<int:line_id>/extensions/<int:extension_id>', methods=['DELETE'])
+def dissociate_line_extension(line_id, extension_id):
+    url.check_line_exists(line_id)
+    url.check_extension_exists(extension_id)
+    model = LineExtension(line_id=line_id, extension_id=extension_id)
+    line_extension_services.dissociate(model)
+    return make_response('', 204)
