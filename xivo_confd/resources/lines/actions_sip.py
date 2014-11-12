e9223410a38177d1ec66bee384cea4eb4f814585
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-11-12 13:12:20 -0500

    refactor line resource to use new converter

--- xivo_confd/resources/lines/actions_sip.py
+++ xivo_confd/resources/lines/actions_sip.py
@@ -16,73 +16,74 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 import logging
-from . import mapper_sip
 
 from flask import Blueprint, url_for
-from flask.globals import request
+from flask import request
 from flask.helpers import make_response
 from xivo_dao.data_handler.line import services as line_services
 from xivo_dao.data_handler.line.model import LineSIP
 from xivo_confd import config
-from xivo_confd.helpers import serializer
 from xivo_confd.helpers.route_generator import RouteGenerator
-from xivo_confd.helpers.formatter import Formatter
 
 from xivo_confd.flask_http_server import content_parser
 from xivo_confd.helpers.mooltiparse import Field, Int, Unicode
+from xivo_confd.helpers.converter import Converter
 
 
 logger = logging.getLogger(__name__)
 blueprint = Blueprint('lines_sip', __name__, url_prefix='/%s/lines_sip' % config.VERSION_1_1)
 route = RouteGenerator(blueprint)
-formatter = Formatter(mapper_sip, serializer, LineSIP)
 
 document = content_parser.document(
     Field('id', Int()),
+    Field('context', Unicode()),
     Field('username', Unicode()),
     Field('secret', Unicode()),
-    Field('callerid', Unicode()),
     Field('provisioning_extension', Unicode()),
     Field('device_slot', Int()),
-    Field('context', Unicode()),
+    Field('callerid', Unicode()),
 )
+
+converter = Converter.for_document(document, LineSIP, 'lines_sip')
 
 
 @route('')
 def list_sip():
     lines = line_services.find_all_by_protocol('sip')
-    result = formatter.list_to_api(lines)
-    return make_response(result, 200)
+    items = converter.encode_list(lines)
+    return make_response(items, 200)
 
 
-@route('/<int:lineid>')
-def get(lineid):
-    line = line_services.get(lineid)
-    result = formatter.to_api(line)
-    return make_response(result, 200)
+@route('/<int:resource_id>')
+def get(resource_id):
+    line = line_services.get(resource_id)
+    encoded_line = converter.encode(line)
+    return make_response(encoded_line, 200)
 
 
 @route('', methods=['POST'])
 def create():
-    data = document.parse(request)
-    line = formatter.dict_to_model(data)
-    line = line_services.create(line)
-    result = formatter.to_api(line)
-    location = url_for('.get', lineid=line.id)
-    return make_response(result, 201, {'Location': location})
+    line = converter.decode(request)
+    line.name = line.username
+
+    created_line = line_services.create(line)
+    encoded_line = converter.encode(created_line)
+    location = url_for('.get', resource_id=created_line.id)
+    return make_response(encoded_line, 201, {'Location': location})
 
 
-@route('/<int:lineid>', methods=['PUT'])
-def edit(lineid):
-    data = document.parse(request)
-    line = line_services.get(lineid)
-    formatter.update_dict_model(data, line)
+@route('/<int:resource_id>', methods=['PUT'])
+def edit(resource_id):
+    line = line_services.get(resource_id)
+    converter.update(request, line)
+    line.name = line.username
+
     line_services.edit(line)
     return make_response('', 204)
 
 
-@route('/<int:lineid>', methods=['DELETE'])
-def delete(lineid):
-    line = line_services.get(lineid)
+@route('/<int:resource_id>', methods=['DELETE'])
+def delete(resource_id):
+    line = line_services.get(resource_id)
     line_services.delete(line)
     return make_response('', 204)
