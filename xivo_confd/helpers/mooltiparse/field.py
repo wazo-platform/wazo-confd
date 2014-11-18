0557049c43c1bd4763d4411ead11710df9ad9dd8
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-11-18 13:39:30 -0500

    add missing license headers

--- xivo_confd/helpers/mooltiparse/field.py
+++ xivo_confd/helpers/mooltiparse/field.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2014 Avencall
@@ -15,6 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 import collections
 
 from errors import ValidationError
