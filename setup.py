4583c687f1f5289f06a6be42db2178037bdc4a64
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-01-22 11:31:54 -0500

    remove unused imports

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-# Copyright (C) 2012-2014  Avencall
+# Copyright (C) 2012-2015  Avencall
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,9 +16,6 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-import fnmatch
-import os
 
 from setuptools import setup
 from setuptools import find_packages
