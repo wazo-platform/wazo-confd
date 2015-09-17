107ca1dc58a40e63a541242ef742559ef88a59a9
Gregory Eric Sanderson <gsanderson@avencall.com>
2015-09-17 15:12:52 -0400

    create 2 seperate helpers for line and line_sip

--- integration_tests/suite/test_api/fixtures.py
+++ integration_tests/suite/test_api/fixtures.py
@@ -1,7 +1,7 @@
 from wrappers import IsolatedAction
 
+from helpers import line, line_sip
 from helpers.user import generate_user, delete_user
-from helpers.line import generate_line, delete_line
 from helpers.extension import generate_extension, delete_extension
 from helpers.device import generate_device, delete_device
 from helpers.voicemail import generate_voicemail, delete_voicemail
@@ -16,8 +16,14 @@ class user(IsolatedAction):
 
 class line(IsolatedAction):
 
-    actions = {'generate': generate_line,
-               'delete': delete_line}
+    actions = {'generate': line.generate_line,
+               'delete': line.delete_line}
+
+
+class line_sip(IsolatedAction):
+
+    actions = {'generate': line_sip.generate_line,
+               'delete': line_sip.delete_line}
 
 
 class extension(IsolatedAction):
--- integration_tests/suite/test_api/helpers/line.py
+++ integration_tests/suite/test_api/helpers/line.py
@@ -3,15 +3,15 @@ from test_api import config
 
 
 def add_line(**params):
-    response = confd.lines_sip.post(params)
+    response = confd.lines.post(params)
     return response.item
 
 
 def delete_line(line_id, check=False):
-    response = confd.lines_sip(line_id).delete()
+    response = confd.lines(line_id).delete()
     if check:
         response.assert_ok()
 
 
 def generate_line():
-    return add_line(context=config.CONTEXT, device_slot=1)
+    return add_line(context=config.CONTEXT)
