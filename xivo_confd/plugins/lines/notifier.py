71c16f702c38d6a1fd8c9eb69abc1837c216ac98
Gregory Eric Sanderson <gsanderson@avencall.com>
2015-10-01 11:30:52 -0400

    rewrite line notifier

---
+++ xivo_confd/plugins/lines/notifier.py
@@ -0,0 +1,57 @@
+# -*- coding: UTF-8 -*-
+
+# Copyright (C) 2015 Avencall
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from xivo_confd.helpers.sysconfd_connector import new_client
+from xivo_confd.helpers import bus_manager
+
+from xivo_bus.resources.line.event import CreateLineEvent, \
+    EditLineEvent, DeleteLineEvent
+
+
+class LineNotifier(object):
+
+    def __init__(self, sysconfd, bus):
+        self.sysconfd = sysconfd
+        self.bus = bus
+
+    def send_sysconfd_handlers(self):
+        handlers = {'ctibus': [],
+                    'dird': [],
+                    'ipbx': ['sip reload'],
+                    'agentbus': []}
+        self.sysconfd.exec_request_handlers(handlers)
+
+    def created(self, line):
+        self.send_sysconfd_handlers()
+        event = CreateLineEvent(line.id)
+        self.bus.send_bus_event(event, event.routing_key)
+
+    def edited(self, line):
+        self.send_sysconfd_handlers()
+        event = EditLineEvent(line.id)
+        self.bus.send_bus_event(event, event.routing_key)
+
+    def deleted(self, line):
+        self.send_sysconfd_handlers()
+        event = DeleteLineEvent(line.id)
+        self.bus.send_bus_event(event, event.routing_key)
+
+
+def build_notifier():
+    return LineNotifier(new_client(),
+                        bus_manager)
