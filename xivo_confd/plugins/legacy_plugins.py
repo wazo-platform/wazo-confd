efbe757ccc0d8d3ab2f827dec6a129be6987a0fa
Gregory Eric Sanderson <gsanderson@avencall.com>
2015-11-24 11:36:36 -0500

    remove old user resource

--- xivo_confd/plugins/legacy_plugins.py
+++ xivo_confd/plugins/legacy_plugins.py
@@ -26,7 +26,6 @@ from xivo_confd.resources.devices import actions as devices_actions
 from xivo_confd.resources.extensions import actions as extensions_actions
 from xivo_confd.resources.infos import actions as infos_actions
 from xivo_confd.resources.queue_members import actions as queue_members_actions
-from xivo_confd.resources.users import actions as users_actions
 from xivo_confd.resources.voicemails import actions as voicemails_actions
 from xivo_confd.resources.user_cti_profile import actions as user_cti_profile_actions
 from xivo_confd.resources.user_voicemail import actions as user_voicemail_actions
@@ -46,7 +45,6 @@ class LegacyPlugins(object):
         self.load_resource(extensions_actions, core)
         self.load_resource(infos_actions, core)
         self.load_resource(queue_members_actions, core)
-        self.load_resource(users_actions, core)
         self.load_resource(voicemails_actions, core)
         self.load_resource(user_cti_profile_actions, core)
         self.load_resource(user_voicemail_actions, core)
