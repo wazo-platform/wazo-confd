260d37f12d8f1d808d5140aef18ac56c65b258cd
Lord Gregory Eric Sanderson Turcot Temlett MacDonnell of Glengary Forbes of Linden <gsanderson@proformatique.com>
2016-02-26 13:29:18 -0500

    add ACLs to line-device URLs

--- xivo_confd/plugins/line_device/resource.py
+++ xivo_confd/plugins/line_device/resource.py
@@ -23,6 +23,8 @@ from flask_restful import fields
 from xivo_confd.helpers.restful import ConfdResource
 from xivo_confd.helpers.restful import Link, FieldList
 
+from xivo_confd.authentication.confd_auth import required_acl
+
 
 fields = {
     'line_id': fields.Integer,
@@ -47,12 +49,14 @@ class LineDevice(ConfdResource):
 
 class LineDeviceAssociation(LineDevice):
 
+    @required_acl('confd.lines.{line_id}.devices.{device_id}.update')
     def put(self, line_id, device_id):
         line = self.line_dao.get(line_id)
         device = self.device_dao.get(device_id)
         self.service.associate(line, device)
         return '', 204
 
+    @required_acl('confd.lines.{line_id}.devices.{device_id}.delete')
     def delete(self, line_id, device_id):
         line = self.line_dao.get(line_id)
         device = self.device_dao.get(device_id)
@@ -62,6 +66,7 @@ class LineDeviceAssociation(LineDevice):
 
 class LineDeviceGet(LineDevice):
 
+    @required_acl('confd.lines.{line_id}.devices.read')
     def get(self, line_id):
         line_device = self.service.get_association_from_line(line_id)
         return marshal(line_device, fields)
@@ -69,6 +74,7 @@ class LineDeviceGet(LineDevice):
 
 class DeviceLineGet(LineDevice):
 
+    @required_acl('confd.devices.{device_id}.lines.read')
     def get(self, device_id):
         device = self.device_dao.get(device_id)
         line_devices = self.service.find_all_associations_from_device(device.id)
