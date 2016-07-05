a455c7e80b6c147105196dab318a5e7c2318927c
Francois Blackburn <fblackburn@proformatique.com>
2016-07-05 11:53:06 -0400

    int tests: use fixtures in a part of funckeys tests

--- integration_tests/suite/base/test_func_keys.py
+++ integration_tests/suite/base/test_func_keys.py
@@ -111,6 +111,169 @@ invalid_destinations = [
 ]
 
 
+def test_put_user_errors():
+    fake_user = confd.users(FAKE_ID).funckeys(1).put
+
+    s.check_resource_not_found(fake_user, 'User')
+
+
+def test_delete_user_errors():
+    fake_user = confd.users(FAKE_ID).funckeys(1).delete
+    s.check_resource_not_found(fake_user, 'User')
+
+    # This should raise an error
+    # fake_funckey = confd.users(self.user['id']).funckeys(FAKE_ID).delete
+    # s.check_resource_not_found(fake_funckey, 'FuncKey')
+
+
+@fixtures.user()
+def test_get_user_errors(user):
+    fake_user_1 = confd.users(FAKE_ID).funckeys.get
+    fake_user_2 = confd.users(FAKE_ID).funckeys(1).get
+    fake_funckey = confd.users(user['uuid']).funckeys(FAKE_ID).get
+
+    s.check_resource_not_found(fake_user_1, 'User')
+    s.check_resource_not_found(fake_user_2, 'User')
+    s.check_resource_not_found(fake_funckey, 'FuncKey')
+
+
+@fixtures.user()
+@fixtures.line_sip()
+@fixtures.extension()
+@fixtures.device()
+def test_list_user(user, line_sip, extension, device):
+    with a.line_extension(line_sip, extension), a.user_line(user, line_sip), a.line_device(line_sip, device):
+        destination_1 = {'type': 'custom', 'exten': '1234'}
+        destination_2 = {'type': 'custom', 'exten': '456'}
+        destination_3 = {'type': 'custom', 'exten': '789'}
+        destination_4 = {'type': 'custom', 'exten': '012'}
+        confd.users(user['id']).funckeys(1).put(destination=destination_1)
+        confd.users(user['id']).funckeys(2).put(destination=destination_2)
+        confd.users(user['id']).funckeys(3).put(destination=destination_3)
+        template_parameters = {'name': 'pos4',
+                               'keys': {'4': {'destination': destination_4}}}
+        template = confd.funckeys.templates.post(**template_parameters).item
+        confd.users(user['id']).funckeys.templates(template['id']).put()
+
+        response = confd.users(user['id']).funckeys.get()
+        expected_result = has_entries({'keys': has_entries({
+            '1': has_entries({'destination': has_entries(destination_1)}),
+            '2': has_entries({'destination': has_entries(destination_2)}),
+            '3': has_entries({'destination': has_entries(destination_3)}),
+            '4': has_entries({'destination': has_entries(destination_4)})})
+        })
+
+        assert_that(response.item, expected_result)
+
+
+@fixtures.user()
+@fixtures.line_sip()
+@fixtures.extension()
+@fixtures.device()
+def test_put_user_position(user, line_sip, extension, device):
+    modified_funckey = {'blf': False,
+                        'label': 'myfunckey',
+                        'destination': {'type': 'park_position',
+                                        'position': 701}}
+    uuid_funckey = {'blf': False,
+                    'label': 'myfunckey',
+                    'destination': {'type': 'park_position',
+                                    'position': 702}}
+
+    provd_funckey = {'label': 'myfunckey',
+                     'type': 'speeddial',
+                     'line': 1,
+                     'value': '701'}
+    provd_uuid_funckey = {'label': 'myfunckey',
+                          'type': 'speeddial',
+                          'line': 1,
+                          'value': '702'}
+    position = '1'
+
+    with a.line_extension(line_sip, extension), a.user_line(user, line_sip), a.line_device(line_sip, device):
+        destination = {'type': 'custom', 'exten': '1234'}
+        confd.users(user['id']).funckeys(position).put(destination=destination)
+
+        response = confd.users(user['id']).funckeys(position).put(modified_funckey)
+        response.assert_updated()
+        check_provd_has_funckey(device, position, provd_funckey)
+
+        response = confd.users(user['uuid']).funckeys(position).put(uuid_funckey)
+        response.assert_updated()
+        check_provd_has_funckey(device, position, provd_uuid_funckey)
+
+
+def check_provd_has_funckey(device, position, funckey):
+    config = provd.configs.get(device['id'])
+    funckeys = config['raw_config']['funckeys']
+
+    assert_that(funckeys, has_key(position))
+    assert_that(funckeys[position], has_entries(funckey))
+
+
+@fixtures.user()
+@fixtures.line_sip(position=2)
+@fixtures.extension()
+@fixtures.device()
+def test_when_line_has_another_position_then_func_key_generated(user, line_sip, extension, device):
+    with a.line_extension(line_sip, extension), a.user_line(user, line_sip), a.line_device(line_sip, device):
+        destination = {'type': 'custom', 'exten': '1234'}
+        confd.users(user['id']).funckeys(1).put(destination=destination).assert_updated()
+        response = confd.users(user['id']).funckeys(1).get()
+        assert_that(response.item['destination'], has_entries(destination))
+
+
+@fixtures.user()
+@fixtures.line_sip()
+@fixtures.extension()
+@fixtures.device()
+def test_delete_user_position(user, line_sip, extension, device):
+    destination_id = {'type': 'custom', 'exten': '1234'}
+    destination_uuid = {'type': 'custom', 'exten': '1235'}
+    position_id = '1'
+    position_uuid = '2'
+    with a.line_extension(line_sip, extension), a.user_line(user, line_sip), a.line_device(line_sip, device):
+        confd.users(user['id']).funckeys(position_id).put(destination=destination_id).assert_updated()
+        confd.users(user['id']).funckeys(position_uuid).put(destination=destination_uuid).assert_updated()
+
+        response = confd.users(user['id']).funckeys(position_id).delete()
+        response.assert_deleted()
+        response = confd.users(user['uuid']).funckeys(position_uuid).delete()
+        response.assert_deleted()
+
+        response = confd.users(user['id']).funckeys(position_id).get().assert_status(404)
+        response = confd.users(user['uuid']).funckeys(position_uuid).get().assert_status(404)
+        check_provd_does_not_have_funckey(device, position_id)
+        check_provd_does_not_have_funckey(device, position_uuid)
+
+
+def check_provd_does_not_have_funckey(device, position):
+    config = provd.configs.get(device['id'])
+    funckeys = config['raw_config']['funckeys']
+
+    assert_that(funckeys, is_not(has_key(position)))
+
+
+@fixtures.user()
+def test_get_user_position(user):
+    funckey = {'label': 'example',
+               'blf': True,
+               'destination': {'type': 'custom',
+                               'exten': '1234'}}
+
+    confd.users(user['id']).funckeys(1).put(funckey).assert_updated()
+
+    expected_funckey = funckey
+    expected_funckey['destination']['href'] = None
+    expected_funckey['inherited'] = False
+
+    response = confd.users(user['id']).funckeys(1).get()
+    assert_that(response.item, has_entries(expected_funckey))
+
+    response = confd.users(user['uuid']).funckeys(1).get()
+    assert_that(response.item, has_entries(expected_funckey))
+
+
 class BaseTestFuncKey(unittest.TestCase):
 
     def setUp(self):
@@ -151,150 +314,6 @@ class BaseTestFuncKey(unittest.TestCase):
     def add_funckey_to_user(self, pos, funckey):
         response = confd.users(self.user['id']).funckeys(pos).put(**funckey)
         response.assert_ok()
-
-
-class TestUserWithFuncKey(BaseTestFuncKey):
-
-    def setUp(self):
-        super(TestUserWithFuncKey, self).setUp()
-
-        self.destination = {'type': 'custom', 'exten': '1234'}
-        self.pos = '1'
-
-        self.funckey_url = confd.users(self.user['id']).funckeys(self.pos)
-        self.uuid_url = confd.users(self.user['uuid']).funckeys(self.pos)
-
-        self.funckey_url.put(destination=self.destination).assert_ok()
-
-    def test_when_line_has_another_position_then_func_key_generated(self):
-        user = helpers.user.generate_user()
-        sip = helpers.endpoint_sip.generate_sip()
-        line = helpers.line.generate_line(position=2)
-        extension = helpers.extension.generate_extension()
-        device = helpers.device.generate_device()
-
-        helpers.line_endpoint_sip.associate(line['id'], sip['id'])
-        helpers.line_extension.associate(line['id'], extension['id'])
-        helpers.user_line.associate(user['id'], line['id'])
-        helpers.line_device.associate(line['id'], device['id'])
-
-        url = confd.users(user['id']).funckeys(self.pos)
-        url.put(destination=self.destination).assert_ok()
-
-        expected_funckey = {'label': None,
-                            'inherited': False,
-                            'blf': True}
-        expected_destination = {'type': self.destination['type'],
-                                'exten': self.destination['exten'],
-                                'href': None}
-
-        response = url.get()
-        assert_that(response.item, has_entries(expected_funckey))
-        assert_that(response.item['destination'], has_entries(expected_destination))
-
-    def test_when_getting_position_then_func_key_returned(self):
-        expected_funckey = {'label': None,
-                            'inherited': False,
-                            'blf': True}
-        expected_destination = {'type': self.destination['type'],
-                                'exten': self.destination['exten'],
-                                'href': None}
-
-        response = self.funckey_url.get()
-
-        assert_that(response.item, has_entries(expected_funckey))
-        assert_that(response.item['destination'], has_entries(expected_destination))
-
-        response = self.uuid_url.get()
-
-        assert_that(response.item, has_entries(expected_funckey))
-        assert_that(response.item['destination'], has_entries(expected_destination))
-
-    def test_when_updating_position_then_func_key_modified_in_provd(self):
-        modified_funckey = {'blf': False,
-                            'label': 'myfunckey',
-                            'destination': {'type': 'park_position',
-                                            'position': 701}}
-        uuid_funckey = {'blf': False,
-                        'label': 'myfunckey',
-                        'destination': {'type': 'park_position',
-                                        'position': 702}}
-
-        provd_funckey = {'label': 'myfunckey',
-                         'type': 'speeddial',
-                         'line': 1,
-                         'value': '701'}
-        provd_uuid_funckey = {'label': 'myfunckey',
-                              'type': 'speeddial',
-                              'line': 1,
-                              'value': '702'}
-
-        self.funckey_url.put(**modified_funckey).assert_updated()
-        self.check_provd_has_funckey(self.pos, provd_funckey)
-
-        self.uuid_url.put(**uuid_funckey).assert_updated()
-        self.check_provd_has_funckey(self.pos, provd_uuid_funckey)
-
-    def test_when_deleting_position_then_func_key_removed(self):
-        response = self.funckey_url.delete()
-        response.assert_ok()
-
-        response = self.funckey_url.get()
-        response.assert_status(404)
-
-        self.check_provd_does_not_have_funckey(self.pos)
-
-    def test_when_deleting_position_using_uuid_then_func_key_removed(self):
-        response = self.uuid_url.delete()
-        response.assert_ok()
-
-        response = self.funckey_url.get()
-        response.assert_status(404)
-
-        self.check_provd_does_not_have_funckey(self.pos)
-
-    def test_get_user_funckeys(self):
-        destination_2 = {'type': 'custom', 'exten': '456'}
-        destination_3 = {'type': 'custom', 'exten': '789'}
-        destination_4 = {'type': 'custom', 'exten': '012'}
-        confd.users(self.user['id']).funckeys(2).put(destination=destination_2)
-        confd.users(self.user['id']).funckeys(3).put(destination=destination_3)
-        template_parameters = {'name': 'pos4',
-                               'keys': {'4': {'destination': destination_4}}}
-        template = confd.funckeys.templates.post(**template_parameters).item
-        confd.users(self.user['id']).funckeys.templates(template['id']).put()
-
-        response = confd.users(self.user['id']).funckeys.get()
-
-        expected_result = has_entries({'keys': has_entries({
-            '1': has_entries({'destination': has_entries(self.destination)}),
-            '2': has_entries({'destination': has_entries(destination_2)}),
-            '3': has_entries({'destination': has_entries(destination_3)}),
-            '4': has_entries({'destination': has_entries(destination_4)})})
-        })
-
-        assert_that(response.item, expected_result)
-
-    def test_put_errors(self):
-        fake_user = confd.users(FAKE_ID).funckeys(1).put
-        s.check_resource_not_found(fake_user, 'User')
-
-    def test_delete_errors(self):
-        fake_user = confd.users(FAKE_ID).funckeys(1).delete
-        s.check_resource_not_found(fake_user, 'User')
-
-        # This should raise an error
-        # fake_funckey = confd.users(self.user['id']).funckeys(FAKE_ID).delete
-        # s.check_resource_not_found(fake_funckey, 'FuncKey')
-
-    def test_get_errors(self):
-        fake_user_1 = confd.users(FAKE_ID).funckeys.get
-        fake_user_2 = confd.users(FAKE_ID).funckeys(1).get
-        fake_funckey = confd.users(self.user['uuid']).funckeys(FAKE_ID).get
-
-        s.check_resource_not_found(fake_user_1, 'User')
-        s.check_resource_not_found(fake_user_2, 'User')
-        s.check_resource_not_found(fake_funckey, 'FuncKey')
 
 
 class TestAllFuncKeyDestinations(BaseTestFuncKey):
