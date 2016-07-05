7e09dbe6f53e5b1a59050bdb0e3417b15d9d133b
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-07-05 15:13:51 -0400

    entity notifier: publish an event on edited

--- xivo_confd/plugins/entity/notifier.py
+++ xivo_confd/plugins/entity/notifier.py
@@ -19,7 +19,8 @@
 from xivo_confd import bus
 
 from xivo_bus.resources.entity.event import (CreateEntityEvent,
-                                             DeleteEntityEvent)
+                                             DeleteEntityEvent,
+                                             EditEntityEvent)
 
 
 class EntityNotifier(object):
@@ -33,6 +34,10 @@ class EntityNotifier(object):
 
     def deleted(self, entity):
         event = DeleteEntityEvent(entity.id)
+        self.bus.send_bus_event(event, event.routing_key)
+
+    def edited(self, entity):
+        event = EditEntityEvent(entity.id)
         self.bus.send_bus_event(event, event.routing_key)
 
 
