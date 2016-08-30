bd404358f4d4259f1198253c637837216a8b82ef
Francois Blackburn <fblackburn@proformatique.com>
2016-08-30 11:06:29 -0400

    sip_general: redefine HTTP interface

--- xivo_confd/plugins/sip_general/resource.py
+++ xivo_confd/plugins/sip_general/resource.py
@@ -16,7 +16,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 from flask import request
-from marshmallow import fields, post_dump, pre_load
+
+from marshmallow import fields, post_dump, pre_load, pre_dump, post_load
 from marshmallow.validate import Length, NoneOf
 
 from xivo_confd.authentication.confd_auth import required_acl
@@ -28,7 +29,7 @@ from xivo_dao.alchemy.staticsip import StaticSIP
 REGISTER_ERROR = "The 'register' key can only be defined in trunk options"
 
 
-class KeyValueOption(BaseSchema):
+class SIPGeneralOption(BaseSchema):
     key = fields.String(validate=(Length(max=128),
                                   NoneOf(['register'], error=REGISTER_ERROR)),
                         required=True,
@@ -38,6 +39,8 @@ class KeyValueOption(BaseSchema):
                           allow_none=True,
                           attribute='var_val')
 
+
+class SIPGeneralOrderedOption(SIPGeneralOption):
     @pre_load
     def add_envelope(self, data):
         if isinstance(data, list) and len(data) == 2:
@@ -49,8 +52,43 @@ class KeyValueOption(BaseSchema):
 
 
 class SIPGeneralSchema(BaseSchema):
-    options = fields.List(fields.Nested(KeyValueOption),
-                          required=True)
+    options = fields.Nested(SIPGeneralOption,
+                            many=True,
+                            required=True)
+
+    ordered_options = fields.List(fields.Nested(SIPGeneralOrderedOption),
+                                  required=True)
+
+    @pre_load
+    def convert_options_to_collection(self, data):
+        options = data.get('options')
+        if isinstance(options, dict):
+            data['options'] = [{'key': key, 'value': value} for key, value in options.iteritems()]
+        return data
+
+    @post_load
+    def merge_options_and_ordered_options(self, data):
+        self._add_metric(data)
+        result = []
+        result.extend(data['options'])
+        result.extend(data['ordered_options'])
+        return result
+
+    def _add_metric(self, data):
+        for metric, ordered_option in enumerate(data['ordered_options']):
+            ordered_option['metric'] = metric
+        for option in data['options']:
+            option['metric'] = None
+
+    @pre_dump
+    def separate_options_and_ordered_options(self, data):
+        return {'options': [option for option in data if option.metric is None],
+                'ordered_options': [option for option in data if option.metric is not None]}
+
+    @post_dump
+    def convert_options_to_dict(self, data):
+        data['options'] = {option['key']: option['value'] for option in data['options']}
+        return data
 
 
 class SIPGeneralList(ConfdResource):
@@ -65,11 +103,11 @@ class SIPGeneralList(ConfdResource):
     @required_acl('confd.asterisk.sip.general.get')
     def get(self):
         options = self.service.list()
-        return self.schema.dump({'options': options}).data
+        return self.schema.dump(options).data
 
     @required_acl('confd.asterisk.sip.general.update')
     def put(self):
         form = self.schema.load(request.get_json()).data
-        sip_general = [StaticSIP(**option) for option in form['options']]
+        sip_general = [StaticSIP(**option) for option in form]
         self.service.edit(sip_general)
         return '', 204
