966c502d1a02a8171831edd144b7e5fe306e6f80
Etienne Lessard <elessard@proformatique.com>
2016-09-07 15:50:37 -0400

    trunk: reload chan_sip/chan_iax2
    On edit, the context might change, so a reload is needed.
    
    On deleted, the endpoint will be deleted if associated, so a reload is
    also needed.

--- xivo_confd/plugins/trunk/notifier.py
+++ xivo_confd/plugins/trunk/notifier.py
@@ -16,7 +16,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from xivo_confd import bus
+from xivo_confd import bus, sysconfd
 
 from xivo_bus.resources.trunk.event import (CreateTrunkEvent,
                                             EditTrunkEvent,
@@ -25,21 +25,37 @@ from xivo_bus.resources.trunk.event import (CreateTrunkEvent,
 
 class TrunkNotifier(object):
 
-    def __init__(self, bus):
+    _IPBX_COMMANDS = {
+        'sip': ['sip reload'],
+        'iax': ['iax2 reload'],
+    }
+
+    def __init__(self, bus, sysconfd):
         self.bus = bus
+        self.sysconfd = sysconfd
+
+    def send_sysconfd_handlers(self, ipbx_commands):
+        handlers = {'ctibus': [],
+                    'ipbx': ipbx_commands,
+                    'agentbus': []}
+        self.sysconfd.exec_request_handlers(handlers)
 
     def created(self, trunk):
         event = CreateTrunkEvent(trunk.id)
         self.bus.send_bus_event(event, event.routing_key)
 
     def edited(self, trunk):
+        if trunk.endpoint in self._IPBX_COMMANDS:
+            self.send_sysconfd_handlers(self._IPBX_COMMANDS[trunk.endpoint])
         event = EditTrunkEvent(trunk.id)
         self.bus.send_bus_event(event, event.routing_key)
 
     def deleted(self, trunk):
+        if trunk.endpoint in self._IPBX_COMMANDS:
+            self.send_sysconfd_handlers(self._IPBX_COMMANDS[trunk.endpoint])
         event = DeleteTrunkEvent(trunk.id)
         self.bus.send_bus_event(event, event.routing_key)
 
 
 def build_notifier():
-    return TrunkNotifier(bus)
+    return TrunkNotifier(bus, sysconfd)
--- xivo_confd/plugins/trunk/tests/test_notifier.py
+++ xivo_confd/plugins/trunk/tests/test_notifier.py
@@ -32,9 +32,10 @@ class TestTrunkNotifier(unittest.TestCase):
 
     def setUp(self):
         self.bus = Mock()
+        self.sysconfd = Mock()
         self.trunk = Mock(Trunk, id=1234)
 
-        self.notifier = TrunkNotifier(self.bus)
+        self.notifier = TrunkNotifier(self.bus, self.sysconfd)
 
     def test_when_trunk_created_then_event_sent_on_bus(self):
         expected_event = CreateTrunkEvent(self.trunk.id)
@@ -59,3 +60,68 @@ class TestTrunkNotifier(unittest.TestCase):
 
         self.bus.send_bus_event.assert_called_once_with(expected_event,
                                                         expected_event.routing_key)
+
+    def test_when_trunk_sip_edited_then_sip_reloaded(self):
+        self.trunk.endpoint = 'sip'
+
+        self.notifier.edited(self.trunk)
+
+        self.sysconfd.exec_request_handlers.assert_called_once_with(self._sysconfd_handlers())
+
+    def test_when_trunk_iax_edited_then_iax_reloaded(self):
+        self.trunk.endpoint = 'iax'
+
+        self.notifier.edited(self.trunk)
+
+        self.sysconfd.exec_request_handlers.assert_called_once_with(self._sysconfd_handlers())
+
+    def test_when_trunk_custom_edited_then_no_reload(self):
+        self.trunk.endpoint = 'custom'
+
+        self.notifier.edited(self.trunk)
+
+        self.sysconfd.exec_request_handlers.assert_not_called()
+
+    def test_when_trunk_without_endpoint_edited_then_no_reload(self):
+        self.notifier.edited(self.trunk)
+
+        self.sysconfd.exec_request_handlers.assert_not_called()
+
+    def test_when_trunk_sip_deleted_then_sip_reloaded(self):
+        self.trunk.endpoint = 'sip'
+
+        self.notifier.deleted(self.trunk)
+
+        self.sysconfd.exec_request_handlers.assert_called_once_with(self._sysconfd_handlers())
+
+    def test_when_trunk_iax_deleted_then_iax_reloaded(self):
+        self.trunk.endpoint = 'iax'
+
+        self.notifier.deleted(self.trunk)
+
+        self.sysconfd.exec_request_handlers.assert_called_once_with(self._sysconfd_handlers())
+
+    def test_when_trunk_custom_deleted_then_no_reload(self):
+        self.trunk.endpoint = 'custom'
+
+        self.notifier.deleted(self.trunk)
+
+        self.sysconfd.exec_request_handlers.assert_not_called()
+
+    def test_when_trunk_without_endpoint_deleted_then_no_reload(self):
+        self.notifier.deleted(self.trunk)
+
+        self.sysconfd.exec_request_handlers.assert_not_called()
+
+    def _sysconfd_handlers(self):
+        if self.trunk.endpoint == 'sip':
+            ipbx_commands = ['sip reload']
+        elif self.trunk.endpoint == 'iax':
+            ipbx_commands = ['iax2 reload']
+        else:
+            raise AssertionError('no sysconfd handlers for endpoint {}'.format(self.trunk.endpoint))
+        return {
+            'ctibus': [],
+            'ipbx': ipbx_commands,
+            'agentbus': []
+        }
