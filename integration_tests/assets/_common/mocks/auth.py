a00c8651e31776a66e55bb3bb3d56704daf550b6
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-10-07 15:23:48 -0400

    wizard tests: add mock auth

---
+++ integration_tests/assets/_common/mocks/auth.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016 Proformatique
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/0.1/token', methods=['POST'])
+def list_requests():
+    return jsonify({'data': {'token': 'the-token'}})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9497, debug=True)
