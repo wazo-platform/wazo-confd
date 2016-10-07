5b77b338f4f8f2d33006b3d34e691e11c94198e6
Francois Blackburn <fblackburn@proformatique.com>
2016-10-07 14:30:27 -0400

    line_extension_legacy: convert to marshmallow

--- xivo_confd/plugins/line_extension/legacy.py
+++ xivo_confd/plugins/line_extension/legacy.py
@@ -19,28 +19,27 @@
 from xivo_dao.helpers.exception import NotFoundError
 from xivo_dao.helpers import errors
 
-from flask import url_for
-from flask_restful import reqparse, fields, marshal
+from flask import url_for, request
+from marshmallow import fields
 
-from xivo_confd.helpers.restful import FieldList, Link, ConfdResource
+from xivo_confd.helpers.restful import ConfdResource
+from xivo_confd.helpers.mallow import BaseSchema, Link, ListLink
 
-fields = {
-    'line_id': fields.Integer,
-    'extension_id': fields.Integer,
-    'links': FieldList(Link('lines',
-                            field='line_id',
-                            target='id'),
-                       Link('extensions',
-                            field='extension_id',
-                            target='id'))
-}
 
-parser = reqparse.RequestParser()
-parser.add_argument('line_id', type=int, required=True, location='view_args')
-parser.add_argument('extension_id', type=int, required=True)
+class LineExtensionLegacySchema(BaseSchema):
+    line_id = fields.Integer(dump_only=True)
+    extension_id = fields.Integer(required=True)
+    links = ListLink(Link('lines',
+                          field='line_id',
+                          target='id'),
+                     Link('extensions',
+                          field='extension_id',
+                          target='id'))
 
 
 class LegacyResource(ConfdResource):
+
+    schema = LineExtensionLegacySchema
 
     def __init__(self, service, line_dao, extension_dao, line_extension_dao):
         super(LegacyResource, self).__init__()
@@ -50,7 +49,7 @@ class LegacyResource(ConfdResource):
         self.line_extension_dao = line_extension_dao
 
     def get_extension_or_fail(self):
-        form = parser.parse_args()
+        form = self.schema().load(request.get_json()).data
         try:
             return self.extension_dao.get(form['extension_id'])
         except NotFoundError:
@@ -62,13 +61,13 @@ class LineExtensionLegacy(LegacyResource):
     def get(self, line_id):
         line = self.line_dao.get(line_id)
         line_extension = self.line_extension_dao.get_by(line_id=line.id)
-        return marshal(line_extension, fields)
+        return self.schema().dump(line_extension).data
 
     def post(self, line_id):
         line = self.line_dao.get(line_id)
         extension = self.get_extension_or_fail()
         line_extension = self.service.associate(line, extension)
-        return marshal(line_extension, fields), 201, self.build_headers(line_extension)
+        return self.schema().dump(line_extension).data, 201, self.build_headers(line_extension)
 
     def delete(self, line_id):
         line = self.line_dao.get(line_id)
@@ -89,4 +88,4 @@ class ExtensionLineLegacy(LegacyResource):
     def get(self, extension_id):
         extension = self.extension_dao.get(extension_id)
         line_extension = self.line_extension_dao.get_by(extension_id=extension.id)
-        return marshal(line_extension, fields)
+        return self.schema().dump(line_extension).data
