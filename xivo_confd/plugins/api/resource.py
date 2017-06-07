83dd1fd8f1cb8bcd6117523413b992be5360f894
Sebastien Duthil <sduthil@wazo.community>
2017-06-07 09:17:15 -0400

    api spec: extract loading code to lib-python

--- xivo_confd/plugins/api/resource.py
+++ xivo_confd/plugins/api/resource.py
@@ -1,7 +1,6 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
-# Copyright (C) 2016 Avencall
-# Copyright (C) 2016 Proformatique Inc.
+# Copyright 2016-2017 The Wazo Authors  (see the AUTHORS file)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,13 +16,13 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-import collections
 import logging
 import yaml
 
 from flask import make_response
 from flask.ext.restful import Resource
-from pkg_resources import resource_string, iter_entry_points
+from xivo.chain_map import ChainMap
+from xivo.rest_api_helpers import load_all_api_specs
 
 logger = logging.getLogger(__name__)
 
@@ -36,25 +35,9 @@ class SwaggerResource(Resource):
         self._enabled_plugins = config['enabled_plugins']
 
     def get(self):
-        api_spec = {}
-        entry_points = (e for e in iter_entry_points(group='xivo_confd.plugins') if e.name in self._enabled_plugins)
-        for entry_point in entry_points:
-            try:
-                spec = yaml.load(resource_string(entry_point.module_name, self.api_filename))
-                api_spec = self.update(api_spec, spec)
-            except IOError:
-                logger.debug('API spec for module "%s" does not exist', entry_point.module_name)
+        api_spec = ChainMap(*load_all_api_specs('xivo_confd.plugins', self.api_filename))
 
         if not api_spec.get('info'):
             return {'error': "API spec does not exist"}, 404
 
         return make_response(yaml.dump(api_spec), 200, {'Content-Type': 'application/x-yaml'})
-
-    def update(self, a, b):
-        for key, value in b.iteritems():
-            if isinstance(value, collections.Mapping):
-                result = self.update(a.get(key, {}), value)
-                a[key] = result
-            else:
-                a[key] = b[key]
-        return a
