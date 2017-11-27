e07c8b86fcea60c32066057301974d99df9a5844
Francois Blackburn <fblackburn@wazo.community>
2017-11-27 08:40:40 -0500

    add outcall_call_permission plugin

---
+++ integration_tests/suite/helpers/helpers/outcall_call_permission.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from . import confd
+
+
+def associate(outcall_id, call_permission_id, check=True):
+    response = confd.outcalls(outcall_id).callpermissions(call_permission_id).put()
+    if check:
+        response.assert_ok()
+
+
+def dissociate(outcall_id, call_permission_id, check=True):
+    response = confd.outcalls(outcall_id).callpermissions(call_permission_id).delete()
+    if check:
+        response.assert_ok()
---
+++ xivo_confd/plugins/outcall_call_permission/notifier.py
@@ -0,0 +1,27 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from xivo_confd import bus
+from xivo_bus.resources.outcall_call_permission.event import (
+    OutcallCallPermissionAssociatedEvent,
+    OutcallCallPermissionDissociatedEvent,
+)
+
+
+class OutcallCallPermissionNotifier(object):
+
+    def __init__(self, bus):
+        self.bus = bus
+
+    def associated(self, outcall, call_permission):
+        event = OutcallCallPermissionAssociatedEvent(outcall.id, call_permission.id)
+        self.bus.send_bus_event(event, event.routing_key)
+
+    def dissociated(self, outcall, call_permission):
+        event = OutcallCallPermissionDissociatedEvent(outcall.id, call_permission.id)
+        self.bus.send_bus_event(event, event.routing_key)
+
+
+def build_notifier():
+    return OutcallCallPermissionNotifier(bus)
