660f609aebef2e1498a0a2624a3cecc3f36a6cda
Francois Blackburn <fblackburn@wazo.community>
2017-11-28 08:09:00 -0500

    remove unused import

--- xivo_confd/__init__.py
+++ xivo_confd/__init__.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2015 Avencall
+# Copyright 2015-2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from werkzeug.local import LocalProxy
-from xivo_confd.application import app
-from xivo_confd.application import api
-from xivo_confd.application import auth
 from xivo_confd.application import setup_app
 from xivo_confd.application import get_bus_publisher
 from xivo_confd.application import get_sysconfd_publisher
