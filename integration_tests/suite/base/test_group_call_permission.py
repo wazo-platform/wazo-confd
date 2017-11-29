80dfda80dc0101c26420519431ecdfa9bf8f23c5
Francois Blackburn <fblackburn@wazo.community>
2017-11-29 18:01:26 -0500

    add group call_permission schema relation

--- integration_tests/suite/base/test_group_call_permission.py
+++ integration_tests/suite/base/test_group_call_permission.py
@@ -2,6 +2,13 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+from hamcrest import (
+    assert_that,
+    contains,
+    empty,
+    has_entries,
+)
+
 from ..helpers import (
     associations as a,
     fixtures,
@@ -74,6 +81,46 @@ def test_dissociate(group, call_permission):
     with a.group_call_permission(group, call_permission, check=False):
         response = confd.groups(group['id']).callpermissions(call_permission['id']).delete()
         response.assert_deleted()
+
+
+@fixtures.group()
+@fixtures.call_permission()
+def test_get_call_permissions_relation(group, call_permission):
+    with a.group_call_permission(group, call_permission):
+        response = confd.groups(group['id']).get()
+        assert_that(response.item['call_permissions'], contains(
+            has_entries(id=call_permission['id'],
+                        name=call_permission['name'])
+        ))
+
+
+@fixtures.group()
+@fixtures.call_permission()
+def test_get_groups_relation(group, call_permission):
+    with a.group_call_permission(group, call_permission):
+        response = confd.callpermissions(call_permission['id']).get()
+        assert_that(response.item['groups'], contains(
+            has_entries(id=group['id'],
+                        name=group['name'])
+        ))
+
+
+@fixtures.group()
+@fixtures.call_permission()
+def test_delete_group_when_group_and_call_permission_associated(group, call_permission):
+    with a.group_call_permission(group, call_permission, check=False):
+        confd.groups(group['id']).delete().assert_deleted()
+        response = confd.callpermissions(call_permission['id']).get()
+        assert_that(response.item['groups'], empty())
+
+
+@fixtures.group()
+@fixtures.call_permission()
+def test_delete_call_permission_when_group_and_call_permission_associated(group, call_permission):
+    with a.group_call_permission(group, call_permission, check=False):
+        confd.callpermissions(call_permission['id']).delete().assert_deleted()
+        response = confd.groups(group['id']).get()
+        assert_that(response.item['call_permissions'], empty())
 
 
 @fixtures.group()
