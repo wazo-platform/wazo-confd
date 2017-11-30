c91e12245e180324a66c5e9d9116ab917eaf011b
Francois Blackburn <fblackburn@wazo.community>
2017-11-29 19:14:15 -0500

    group_schedule: remove 400 on di/association

--- xivo_confd/plugins/group_schedule/validator.py
+++ xivo_confd/plugins/group_schedule/validator.py
@@ -10,29 +10,18 @@ from xivo_confd.helpers.validator import ValidatorAssociation, ValidationAssocia
 class GroupScheduleAssociationValidator(ValidatorAssociation):
 
     def validate(self, group, schedule):
-        self.validate_group_not_already_associated(group)
+        if schedule in group.schedules:
+            return
+        self.validate_group_not_already_associated(group, schedule)
 
-    def validate_group_not_already_associated(self, group):
+    def validate_group_not_already_associated(self, group, schedule):
         if group.schedules:
             raise errors.resource_associated('Group', 'Schedule',
                                              group_id=group.id,
                                              schedule_id=group.schedules[0].id)
 
 
-class GroupScheduleDissociationValidator(ValidatorAssociation):
-
-    def validate(self, group, schedule):
-        self.validate_group_schedule_exists(group, schedule)
-
-    def validate_group_schedule_exists(self, group, schedule):
-        if group not in schedule.groups:
-            raise errors.not_found('GroupSchedule',
-                                   group_id=group.id,
-                                   schedule_id=schedule.id)
-
-
 def build_validator():
     return ValidationAssociation(
         association=[GroupScheduleAssociationValidator()],
-        dissociation=[GroupScheduleDissociationValidator()]
     )
