3d4b9a32b29ca9b37d116b19566aa70eca2ce962
Francois Blackburn <fblackburn@wazo.community>
2017-12-07 13:21:56 -0500

    user_entity: remove 400 on di/association

--- xivo_confd/plugins/user_entity/validator.py
+++ xivo_confd/plugins/user_entity/validator.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016 Avencall
-# Copyright (C) 2016 Proformatique Inc.
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from xivo_confd.helpers.validator import ValidationAssociation, ValidatorAssociation
@@ -14,13 +13,9 @@ class UserEntityAssociationValidator(ValidatorAssociation):
         self.user_line_dao = user_line_dao
 
     def validate(self, user, entity):
-        self.validate_user_not_already_associated(user, entity)
-        self.validate_user_no_line_associated(user)
-
-    def validate_user_not_already_associated(self, user, entity):
         if user.entity_id == entity.id:
-            raise errors.resource_associated('User', 'Entity',
-                                             user_id=user.id, entity=entity.id)
+            return
+        self.validate_user_no_line_associated(user)
 
     def validate_user_no_line_associated(self, user):
         user_lines = self.user_line_dao.find_all_by(user_id=user.id)
