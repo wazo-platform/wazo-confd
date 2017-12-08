9a787c4705f429b7d62c4bcc5931535fc5dba432
Francois Blackburn <fblackburn@wazo.community>
2017-12-08 08:20:36 -0500

    remove unused helpers

--- xivo_confd/helpers/sysconfd_connector.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-# Copyright (C) 2015 Avencall
-# SPDX-License-Identifier: GPL-3.0+
-
-from xivo_confd import sysconfd
-
-
-def exec_request_handlers(args):
-    sysconfd.exec_request_handlers(args)
