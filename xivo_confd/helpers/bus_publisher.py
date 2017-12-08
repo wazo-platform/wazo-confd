7a3cf9c11adc318c3bc9d11b7861a60fee92fe19
Francois Blackburn <fblackburn@wazo.community>
2017-12-08 08:56:59 -0500

    use Publisher from xivo_bus

--- xivo_confd/helpers/bus_publisher.py
+++ xivo_confd/helpers/bus_publisher.py
@@ -1,7 +1,11 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2015-2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
 import logging
 
 from kombu import Connection, Exchange, Producer
-from xivo_bus import Marshaler
+from xivo_bus import Marshaler, Publisher
 
 logger = logging.getLogger(__name__)
 
@@ -10,45 +14,25 @@ class BusPublisher(object):
 
     @classmethod
     def from_config(cls, config):
-        url = 'amqp://{username}:{password}@{host}:{port}//'.format(**config['bus'])
-        exchange_name = config['bus']['exchange_name']
-        exchange_type = config['bus']['exchange_type']
-        uuid = config['uuid']
-        return cls(url, exchange_name, exchange_type, uuid)
+        bus_url = 'amqp://{username}:{password}@{host}:{port}//'.format(**config['bus'])
+        bus_connection = Connection(bus_url)
+        bus_exchange = Exchange(config['bus']['exchange_name'], type=config['bus']['exchange_type'])
+        bus_producer = Producer(bus_connection, exchange=bus_exchange, auto_declare=True)
+        bus_marshaler = Marshaler(config['uuid'])
+        bus_publisher = Publisher(bus_producer, bus_marshaler)
+        return cls(bus_publisher)
 
-    def __init__(self, url, exchange_name, exchange_type, uuid):
-        self.url = url
-        self.exchange_name = exchange_name
-        self.exchange_type = exchange_type
-        self.uuid = uuid
+    def __init__(self, publisher):
+        self._publisher = publisher
         self.messages = []
 
     def send_bus_event(self, message, routing_key, headers=None):
-        self.publish(message, routing_key, headers)
-
-    def publish(self, message, routing_key, headers=None):
+        # XXX remove routing_key from all send_bus_event call
         self.messages.append((message, routing_key, headers))
 
     def flush(self):
-        exchange = Exchange(self.exchange_name, self.exchange_type)
-        marshaler = Marshaler(self.uuid)
-        with Connection(self.url) as connection:
-            producer = Producer(connection, exchange=exchange, auto_declare=True)
-            publish = connection.ensure(producer, producer.publish,
-                                        errback=self.publish_error, max_retries=3,
-                                        interval_start=1)
-            self.send_messages(marshaler, publish)
-
-    def send_messages(self, marshaler, publish):
         for event, routing_key, headers in self.messages:
-            message = marshaler.marshal_message(event)
-            all_headers = dict(marshaler.metadata(event))
-            all_headers.update(headers or {})
-            publish(message, content_type=marshaler.content_type, routing_key=routing_key, headers=all_headers)
-
-    def publish_error(self, exc, interval):
-        logger.error('Error: %s', exc, exc_info=1)
-        logger.info('Retry in %s seconds...', interval)
+            self._publisher.publish(event, headers)
 
     def rollback(self):
         self.messages = []
