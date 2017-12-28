98abac003ac8aac1bf528e13bf48b8b419c272ed
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-12-28 11:59:51 -0500

    fix copyright notice

--- xivo_confd/database/wizard.py
+++ xivo_confd/database/wizard.py
@@ -1,6 +1,5 @@
 # -*- coding: UTF-8 -*-
 # Copyright 2016-2017 The Wazo Authors  (see the AUTHORS file)
-# Copyright (C) 2016 Proformatique, Inc.
 # SPDX-License-Identifier: GPL-3.0+
 
 from xivo_dao.alchemy.context import Context
