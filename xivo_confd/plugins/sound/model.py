f716c568425312d5ea336ab34c2eaa82823279e2
Francois Blackburn <fblackburn@wazo.community>
2017-12-29 10:10:40 -0500

    sound: add helpers to model to update files

--- xivo_confd/plugins/sound/model.py
+++ xivo_confd/plugins/sound/model.py
@@ -9,12 +9,35 @@ class SoundCategory(object):
         self.name = name
         self.files = files if files else []
 
+    def add_file(self, new_file):
+        for file_ in self.files:
+            if file_.name == new_file.name:
+                file_.update(new_file)
+                break
+        else:
+            self.files.append(new_file)
+
 
 class SoundFile(object):
 
     def __init__(self, name=None, formats=None):
         self.name = name
         self.formats = formats if formats else []
+
+    def update(self, other_file):
+        if other_file.name != self.name:
+            return
+        self._update_formats(other_file.formats)
+
+    def _update_formats(self, other_formats):
+        new_formats = []
+        for other_format in other_formats:
+            for format_ in self.formats:
+                if other_format == format_:
+                    break
+            else:
+                new_formats.append(other_format)
+        self.formats = self.formats + new_formats
 
 
 class SoundFormat(object):
@@ -23,3 +46,8 @@ class SoundFormat(object):
         self.format = format_
         self.language = language
         self.text = text
+
+    def __eq__(self, other):
+        return (self.format == other.format
+                and self.language == other.language
+                and self.text == other.text)
