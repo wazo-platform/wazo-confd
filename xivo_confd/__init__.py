ced99aa6482f032ba3942364526988e34c4828e0
Francois Blackburn <fblackburn@wazo.community>
2018-01-04 21:56:56 -0500

    clearify LocalProxy from werkzeug
    import LocalProxy as Proxy to say that LocalProxy is only used like a
    'normal' proxy and not like a proxy of the "Local" object of werkzeug

--- xivo_confd/__init__.py
+++ xivo_confd/__init__.py
@@ -2,9 +2,9 @@
 # Copyright 2015-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-from werkzeug.local import LocalProxy
+from werkzeug.local import LocalProxy as Proxy
 from .http_server import get_bus_publisher
 from .http_server import get_sysconfd_publisher
 
-bus = LocalProxy(get_bus_publisher)
-sysconfd = LocalProxy(get_sysconfd_publisher)
+bus = Proxy(get_bus_publisher)
+sysconfd = Proxy(get_sysconfd_publisher)
