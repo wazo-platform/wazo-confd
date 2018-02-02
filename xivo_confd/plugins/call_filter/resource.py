38548a56db8d8c2989bbf8fb0e2192aec48725ae
Francois Blackburn <fblackburn@wazo.community>
2018-02-02 11:29:16 -0500

    fix call_filter import

--- xivo_confd/plugins/call_filter/resource.py
+++ xivo_confd/plugins/call_filter/resource.py
@@ -6,7 +6,7 @@ from flask import url_for
 
 from xivo_dao.alchemy.callfilter import Callfilter as CallFilter
 
-from xivo_confd.authentication.confd_auth import required_acl
+from xivo_confd.auth import required_acl
 from xivo_confd.helpers.restful import ListResource, ItemResource
 
 from .schema import CallFilterSchema
