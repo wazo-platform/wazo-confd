d7a3dc9f00bbaba9504736570519f3a1bc969e29
Francois Blackburn <fblackburn@wazo.community>
2018-02-11 10:23:14 -0500

    features: do not send sysconfd if no change

--- xivo_confd/plugins/extension_feature/service.py
+++ xivo_confd/plugins/extension_feature/service.py
@@ -1,7 +1,8 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+from xivo_dao.helpers.db_manager import Session
 from xivo_dao.resources.extension import dao as extension_dao
 
 from xivo_confd.helpers.resource import CRUDService
@@ -18,6 +19,12 @@ class ExtensionService(CRUDService):
 
     def get(self, resource_id):
         return self.dao.get_by(id=resource_id, is_feature=True)
+
+    def edit(self, resource, updated_fields=None):
+        with Session.no_autoflush:
+            self.validator.validate_edit(resource)
+        self.dao.edit(resource)
+        self.notifier.edited(resource, updated_fields)
 
 
 def build_service():
