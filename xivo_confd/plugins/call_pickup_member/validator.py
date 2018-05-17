67b0e0bf72a59f84e2c1f8795b8ea526f6aa8444
Francois Blackburn <fblackburn@wazo.community>
2018-05-17 11:33:32 -0400

    call_pickup_member: remove unused validator
    It's only a copy/paste error

--- xivo_confd/plugins/call_pickup_member/validator.py
+++ xivo_confd/plugins/call_pickup_member/validator.py
@@ -17,45 +17,13 @@ class CallPickupUserValidator(ValidatorAssociation):
             raise errors.not_permitted('Cannot associate same user more than once')
 
 
-class CallPickupInterceptorUserAssociationValidator(CallPickupUserValidator):
-
-    def validate(self, call_pickup, users):
-        super(CallPickupInterceptorUserAssociationValidator, self).validate(call_pickup, users)
-        self.validate_no_interceptor_user(call_pickup, users)
-
-    def validate_no_interceptor_user(self, call_pickup, users):
-        for interceptor in call_pickup.interceptors:
-            if interceptor.user in users:
-                raise errors.resource_associated(
-                    'CallPickup',
-                    call_pickup.id,
-                    interceptor_user_uuid=interceptor.user.uuid,
-                )
-
-
-class CallPickupTargetUserAssociationValidator(CallPickupUserValidator):
-
-    def validate(self, call_pickup, users):
-        super(CallPickupTargetUserAssociationValidator, self).validate(call_pickup, users)
-        self.validate_no_target_user(call_pickup, users)
-
-    def validate_no_target_user(self, call_pickup, users):
-        for target in call_pickup.targets:
-            if target.user in users:
-                raise errors.resource_associated(
-                    'CallPickup',
-                    call_pickup.id,
-                    target_user_uuid=target.user.uuid,
-                )
-
-
 def build_validator_target_user():
     return ValidationAssociation(
-        association=[CallPickupInterceptorUserAssociationValidator()],
+        association=[CallPickupUserValidator()],
     )
 
 
 def build_validator_interceptor_user():
     return ValidationAssociation(
-        association=[CallPickupTargetUserAssociationValidator()],
+        association=[CallPickupUserValidator()],
     )
