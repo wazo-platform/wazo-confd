c6e12c4183b651531eeb188af1adc1603b86d120
Francois Blackburn <fblackburn@wazo.community>
2018-07-04 10:10:58 -0400

    group_member: extract schemas into module

--- xivo_confd/plugins/group_member/resource.py
+++ xivo_confd/plugins/group_member/resource.py
@@ -3,56 +3,14 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 from flask import request
-from marshmallow import fields, post_load
 
 from xivo_dao.helpers import errors
 from xivo_dao.helpers.exception import NotFoundError
 
 from xivo_confd.auth import required_acl
-from xivo_confd.helpers.mallow import BaseSchema
 from xivo_confd.helpers.restful import ConfdResource
 
-
-class GroupUserSchema(BaseSchema):
-    uuid = fields.String(required=True)
-    priority = fields.Integer()
-
-    @post_load
-    def add_envelope(self, data):
-        data['user'] = {'uuid': data.pop('uuid')}
-        return data
-
-
-class GroupUsersSchema(BaseSchema):
-    users = fields.Nested(GroupUserSchema, many=True, required=True)
-
-    @post_load
-    def set_default_priority(self, data):
-        for priority, user in enumerate(data['users']):
-            user['priority'] = user.get('priority', priority)
-        return data
-
-
-class GroupExtensionSchema(BaseSchema):
-    exten = fields.String(required=True)
-    context = fields.String(required=True)
-    priority = fields.Integer()
-
-    @post_load
-    def add_envelope(self, data):
-        data['extension'] = {'exten': data.pop('exten'),
-                             'context': data.pop('context')}
-        return data
-
-
-class GroupExtensionsSchema(BaseSchema):
-    extensions = fields.Nested(GroupExtensionSchema, many=True, required=True)
-
-    @post_load
-    def set_default_priority(self, data):
-        for priority, extension in enumerate(data['extensions']):
-            extension['priority'] = extension.get('priority', priority)
-        return data
+from .schema import GroupUsersSchema, GroupExtensionsSchema
 
 
 class GroupMemberItem(ConfdResource):
---
+++ xivo_confd/plugins/group_member/schema.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from marshmallow import fields, post_load
+from xivo_confd.helpers.mallow import BaseSchema
+
+
+class GroupUserSchema(BaseSchema):
+    uuid = fields.String(required=True)
+    priority = fields.Integer()
+
+    @post_load
+    def add_envelope(self, data):
+        data['user'] = {'uuid': data.pop('uuid')}
+        return data
+
+
+class GroupUsersSchema(BaseSchema):
+    users = fields.Nested(GroupUserSchema, many=True, required=True)
+
+    @post_load
+    def set_default_priority(self, data):
+        for priority, user in enumerate(data['users']):
+            user['priority'] = user.get('priority', priority)
+        return data
+
+
+class GroupExtensionSchema(BaseSchema):
+    exten = fields.String(required=True)
+    context = fields.String(required=True)
+    priority = fields.Integer()
+
+    @post_load
+    def add_envelope(self, data):
+        data['extension'] = {'exten': data.pop('exten'),
+                             'context': data.pop('context')}
+        return data
+
+
+class GroupExtensionsSchema(BaseSchema):
+    extensions = fields.Nested(GroupExtensionSchema, many=True, required=True)
+
+    @post_load
+    def set_default_priority(self, data):
+        for priority, extension in enumerate(data['extensions']):
+            extension['priority'] = extension.get('priority', priority)
+        return data
