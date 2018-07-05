d4247fd2d6e6a6613439c9c91b425890d1cadcc1
Pascal Cadotte Michaud <pcm@wazo.io>
2018-07-05 13:37:12 -0400

    incall extension: check that the incall and extension are visible

--- xivo_confd/plugins/incall_extension/resource.py
+++ xivo_confd/plugins/incall_extension/resource.py
@@ -8,6 +8,8 @@ from xivo_confd.helpers.restful import ConfdResource
 
 class IncallExtensionItem(ConfdResource):
 
+    has_tenant_uuid = True
+
     def __init__(self, service, incall_dao, extension_dao):
         super(IncallExtensionItem, self).__init__()
         self.service = service
@@ -23,7 +25,10 @@ class IncallExtensionItem(ConfdResource):
 
     @required_acl('confd.incalls.{incall_id}.extensions.{extension_id}.update')
     def put(self, incall_id, extension_id):
-        incall = self.incall_dao.get(incall_id)
-        extension = self.extension_dao.get(extension_id)
+        tenant_uuids = self._build_tenant_list({'recurse': True})
+
+        incall = self.incall_dao.get(incall_id, tenant_uuids=tenant_uuids)
+        extension = self.extension_dao.get(extension_id, tenant_uuids=tenant_uuids)
+
         self.service.associate(incall, extension)
         return '', 204
