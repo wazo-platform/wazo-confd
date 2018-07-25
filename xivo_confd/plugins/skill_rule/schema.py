d1c37b25d0448ba985480524d1284494d78e6148
Francois Blackburn <fblackburn@wazo.community>
2018-07-25 08:32:45 -0400

    skill_rule: make name required
    Also validate name against regex

--- xivo_confd/plugins/skill_rule/schema.py
+++ xivo_confd/plugins/skill_rule/schema.py
@@ -3,14 +3,16 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 from marshmallow import fields, post_load, pre_dump
-from marshmallow.validate import Length
+from marshmallow.validate import Length, Regexp
 
 from xivo_confd.helpers.mallow import BaseSchema, Link, ListLink
+
+NAME_REGEX = r'^[-_.a-zA-Z0-9]+$'
 
 
 class SkillRuleSchema(BaseSchema):
     id = fields.Integer(dump_only=True)
-    name = fields.String(validate=Length(max=64))
+    name = fields.String(validate=(Regexp(NAME_REGEX), Length(max=64)), required=True)
     rules = fields.Nested('SkillRuleRuleSchema', many=True, allow_none=True)
     links = ListLink(Link('skillrules'))
 
