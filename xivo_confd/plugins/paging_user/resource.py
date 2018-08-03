1748e0b2301fbd5243fa2fd993cfe0d038231f1b
Francois Blackburn <fblackburn@wazo.community>
2018-08-03 10:29:41 -0400

    paging_user: make association with member multi-tenant

--- xivo_confd/plugins/paging_user/resource.py
+++ xivo_confd/plugins/paging_user/resource.py
@@ -45,10 +45,11 @@ class PagingMemberUserItem(PagingUserItem):
 
     @required_acl('confd.pagings.{paging_id}.members.users.update')
     def put(self, paging_id):
-        paging = self.paging_dao.get(paging_id)
+        tenant_uuids = self._build_tenant_list({'recurse': True})
+        paging = self.paging_dao.get(paging_id, tenant_uuids=tenant_uuids)
         form = self.schema().load(request.get_json()).data
         try:
-            users = [self.user_dao.get_by(uuid=user['uuid']) for user in form['users']]
+            users = [self.user_dao.get_by(uuid=user['uuid'], tenant_uuids=tenant_uuids) for user in form['users']]
         except NotFoundError as e:
             raise errors.param_not_found('users', 'User', **e.metadata)
 
