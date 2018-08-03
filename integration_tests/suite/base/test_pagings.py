a203a19f77e0ba9ddf8e05062fcc06f6c9d6602a
Francois Blackburn <fblackburn@wazo.community>
2018-08-03 09:51:04 -0400

    paging: test that list is multi-tenant

--- integration_tests/suite/base/test_pagings.py
+++ integration_tests/suite/base/test_pagings.py
@@ -3,11 +3,13 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 from hamcrest import (
+    all_of,
     assert_that,
     empty,
     has_entries,
     has_entry,
     has_item,
+    has_items,
     is_not,
     not_,
 )
@@ -17,6 +19,10 @@ from ..helpers import (
     errors as e,
     fixtures,
     scenarios as s,
+)
+from ..helpers.config import (
+    MAIN_TENANT,
+    SUB_TENANT,
 )
 
 
@@ -128,6 +134,28 @@ def test_sort_offset_limit(paging1, paging2):
     yield s.check_offset_legacy, url, paging1, paging2, 'name', 'sort'
 
     yield s.check_limit, url, paging1, paging2, 'name', 'sort'
+
+
+@fixtures.paging(wazo_tenant=MAIN_TENANT)
+@fixtures.paging(wazo_tenant=SUB_TENANT)
+def test_list_multi_tenant(main, sub):
+    response = confd.pagings.get(wazo_tenant=MAIN_TENANT)
+    assert_that(
+        response.items,
+        all_of(has_item(main)), not_(has_item(sub)),
+    )
+
+    response = confd.pagings.get(wazo_tenant=SUB_TENANT)
+    assert_that(
+        response.items,
+        all_of(has_item(sub), not_(has_item(main))),
+    )
+
+    response = confd.pagings.get(wazo_tenant=MAIN_TENANT, recurse=True)
+    assert_that(
+        response.items,
+        has_items(main, sub),
+    )
 
 
 @fixtures.paging()
