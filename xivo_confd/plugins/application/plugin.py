c4b55261aa3f083149d3fd3fe14a351959b6be27
Francois Blackburn <fblackburn@wazo.community>
2018-08-29 13:15:25 -0400

    add application plugin
    multi-tenant need to be implemented

---
+++ xivo_confd/plugins/application/plugin.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from .resource import ApplicationItem, ApplicationList
+from .service import build_service
+
+
+class Plugin(object):
+
+    def load(self, dependencies):
+        api = dependencies['api']
+        service = build_service()
+
+        api.add_resource(
+            ApplicationList,
+            '/applications',
+            resource_class_args=(service,)
+        )
+
+        api.add_resource(
+            ApplicationItem,
+            '/applications/<uuid:application_uuid>',
+            endpoint='applications',
+            resource_class_args=(service,)
+        )
