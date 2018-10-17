0adb3394e5eef5daeb83c3b2c5c3d5f517a1bafd
Sylvain Boily <sylvain@wazo.io>
2018-10-16 21:28:45 -0400

    add sysconfd notifier when context is included to another context

--- xivo_confd/plugins/context_context/notifier.py
+++ xivo_confd/plugins/context_context/notifier.py
@@ -4,19 +4,27 @@
 
 from xivo_bus.resources.context_context.event import ContextContextsAssociatedEvent
 
-from xivo_confd import bus
+from xivo_confd import bus, sysconfd
 
 
 class ContextContextNotifier(object):
 
-    def __init__(self, bus):
+    def __init__(self, bus, sysconfd):
         self.bus = bus
+        self.sysconfd = sysconfd
+
+    def send_sysconfd_handlers(self):
+        handlers = {'ctibus': [],
+                    'ipbx': ['dialplan reload'],
+                    'agentbus': []}
+        self.sysconfd.exec_request_handlers(handlers)
 
     def associated_contexts(self, context, contexts):
+        self.send_sysconfd_handlers()
         context_ids = [context.id for context in contexts]
         event = ContextContextsAssociatedEvent(context.id, context_ids)
         self.bus.send_bus_event(event)
 
 
 def build_notifier():
-    return ContextContextNotifier(bus)
+    return ContextContextNotifier(bus, sysconfd)
--- xivo_confd/plugins/context_context/tests/test_notifier.py
+++ xivo_confd/plugins/context_context/tests/test_notifier.py
@@ -11,6 +11,12 @@ from xivo_dao.alchemy.context import Context
 
 from ..notifier import ContextContextNotifier
 
+EXPECTED_HANDLERS = {
+    'ctibus': [],
+    'ipbx': ['dialplan reload'],
+    'agentbus': [],
+}
+
 
 class TestContextContextNotifier(unittest.TestCase):
 
@@ -20,7 +26,7 @@ class TestContextContextNotifier(unittest.TestCase):
         self.context = Mock(Context, id=2)
         self.context = Mock(Context, id=1)
 
-        self.notifier = ContextContextNotifier(self.bus)
+        self.notifier = ContextContextNotifier(self.bus, self.sysconfd)
 
     def test_associate_then_bus_event(self):
         expected_event = ContextContextsAssociatedEvent(self.context.id, [self.context.id])
@@ -28,3 +34,8 @@ class TestContextContextNotifier(unittest.TestCase):
         self.notifier.associated_contexts(self.context, [self.context])
 
         self.bus.send_bus_event.assert_called_once_with(expected_event)
+
+    def test_associate_then_sysconfd_event(self):
+        self.notifier.associated_contexts(self.context, [self.context])
+
+        self.sysconfd.exec_request_handlers.assert_called_once_with(EXPECTED_HANDLERS)
