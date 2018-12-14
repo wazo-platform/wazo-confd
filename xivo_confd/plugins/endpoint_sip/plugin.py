bd6989c1724143d7d8a549baf32afd17a241da48
Alexandre Fournier <afournier@wazo.io>
2018-12-14 15:32:52 -0500

    endpoint_sip: use new provd client

--- xivo_confd/plugins/endpoint_sip/plugin.py
+++ xivo_confd/plugins/endpoint_sip/plugin.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 from xivo_provd_client import new_provisioning_client_from_config
+from wazo_provd_client import Client as ProvdClient
 
 from .resource import SipItem, SipList
 from .service import build_service
@@ -13,7 +14,7 @@ class Plugin(object):
     def load(self, dependencies):
         api = dependencies['api']
         config = dependencies['config']
-        provd_client = new_provisioning_client_from_config(config['provd'])
+        provd_client = ProvdClient(**config['provd'])
 
         service = build_service(provd_client)
 
