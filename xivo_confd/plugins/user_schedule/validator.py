93ca3fac134df73962e2d624d34f34ce3d4c9dd6
Francois Blackburn <fblackburn@wazo.community>
2019-01-16 15:24:28 -0500

    user_schedule: make the association multi-tenant

--- xivo_confd/plugins/user_schedule/validator.py
+++ xivo_confd/plugins/user_schedule/validator.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_dao.helpers import errors
@@ -10,13 +10,24 @@ from xivo_confd.helpers.validator import ValidatorAssociation, ValidationAssocia
 class UserScheduleAssociationValidator(ValidatorAssociation):
 
     def validate(self, user, schedule):
+        self.validate_same_tenant(user, schedule)
         self.validate_user_not_already_associated(user)
+
+    def validate_same_tenant(self, user, schedule):
+        if user.tenant_uuid != schedule.tenant_uuid:
+            raise errors.different_tenants(
+                user_tenant_uuid=user.tenant_uuid,
+                schedule_tenant_uuid=schedule.tenant_uuid,
+            )
 
     def validate_user_not_already_associated(self, user):
         if user.schedules:
-            raise errors.resource_associated('User', 'Schedule',
-                                             user_id=user.id,
-                                             schedule_id=user.schedules[0].id)
+            raise errors.resource_associated(
+                'User',
+                'Schedule',
+                user_id=user.id,
+                schedule_id=user.schedules[0].id,
+            )
 
 
 def build_validator():
