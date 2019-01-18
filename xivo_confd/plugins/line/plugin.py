e74e7c41fa07baa703663719a9ec89b68d772c0c
Pascal Cadotte Michaud <pcm@wazo.io>
2019-01-18 08:40:45 -0500

    lines: set the tokon on the ProvdClient

--- xivo_confd/plugins/line/plugin.py
+++ xivo_confd/plugins/line/plugin.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_provd_client import Client as ProvdClient
@@ -13,7 +13,10 @@ class Plugin(object):
     def load(self, dependencies):
         api = dependencies['api']
         config = dependencies['config']
+        token_changed_subscribe = dependencies['token_changed_subscribe']
+
         provd_client = ProvdClient(**config['provd'])
+        token_changed_subscribe(provd_client.set_token)
 
         service = build_service(provd_client)
 
