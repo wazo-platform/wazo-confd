a5b9e2abc8febf7f75387a25e915c43ffc77899a
Sebastien Duthil <sduthil@wazo.community>
2019-02-12 09:18:50 -0500

    dhcp: add missing files

---
+++ xivo_confd/plugins/dhcp/notifier.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from xivo_bus.resources.dhcp.event import (
+    EditDHCPEvent,
+)
+
+
+class DHCPNotifier(object):
+
+    def __init__(self, bus, sysconfd):
+        self.bus = bus
+        self.sysconfd = sysconfd
+
+    def edited(self):
+        event = EditDHCPEvent()
+        self.bus.send_bus_event(event)
+        self.sysconfd.commonconf_generate()
+        self.sysconfd.commonconf_apply()
---
+++ xivo_confd/plugins/dhcp/tests/test_notifier.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import unittest
+
+from mock import Mock
+from xivo_bus.resources.dhcp.event import (
+    EditDHCPEvent,
+)
+from xivo_dao.alchemy.dhcp import Dhcp
+
+from ..notifier import DHCPNotifier
+
+
+class TestDHCPNotifier(unittest.TestCase):
+
+    def setUp(self):
+        self.bus = Mock()
+        self.sysconfd = Mock()
+        self.dhcp = Mock(Dhcp)
+
+        self.notifier = DHCPNotifier(self.bus, self.sysconfd)
+
+    def test_when_dhcp_edited_then_event_sent_on_bus(self):
+        expected_event = EditDHCPEvent()
+
+        self.notifier.edited()
+
+        self.bus.send_bus_event.assert_called_once_with(expected_event)
+        self.sysconfd.commonconf_generate.assert_called_once_with()
+        self.sysconfd.commonconf_apply.assert_called_once_with()
