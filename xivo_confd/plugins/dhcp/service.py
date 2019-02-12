ff00e19523d7cefc96387e6dda21fb57a4c6fe75
Sebastien Duthil <sduthil@wazo.community>
2019-02-12 09:19:43 -0500

    dhcp: extract db operations to database/

--- xivo_confd/plugins/dhcp/service.py
+++ xivo_confd/plugins/dhcp/service.py
@@ -2,8 +2,7 @@
 # Copyright 2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from xivo_dao.alchemy.dhcp import Dhcp
-from xivo_dao.helpers.db_manager import Session
+from xivo_confd.database import dhcp as dhcp_dao
 
 
 class DHCPService(object):
@@ -12,10 +11,8 @@ class DHCPService(object):
         self.notifier = notifier
 
     def get(self):
-        return Session.query(Dhcp).first()
+        return dhcp_dao.get()
 
     def edit(self, form):
-        dhcp = Session.query(Dhcp).first()
-        for name, value in form.iteritems():
-            setattr(dhcp, name, value)
+        dhcp_dao.update(form)
         self.notifier.edited()
