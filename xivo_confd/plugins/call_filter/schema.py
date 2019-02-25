c5575bff897aadf55292433177462a54199f6e82
Francois Blackburn <fblackburn@wazo.community>
2019-02-25 10:39:08 -0500

    fix styling issues

--- xivo_confd/plugins/call_filter/schema.py
+++ xivo_confd/plugins/call_filter/schema.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from marshmallow import fields, post_dump
@@ -9,9 +9,11 @@ from xivo_confd.helpers.mallow import BaseSchema, StrictBoolean, Link, ListLink
 
 
 class CallFilterRecipientsSchema(BaseSchema):
-    user = fields.Nested('UserSchema',
-                         only=['uuid', 'firstname', 'lastname', 'links'],
-                         dump_only=True)
+    user = fields.Nested(
+        'UserSchema',
+        only=['uuid', 'firstname', 'lastname', 'links'],
+        dump_only=True,
+    )
     timeout = fields.Integer(dump_only=True)
 
     @post_dump
@@ -23,9 +25,11 @@ class CallFilterRecipientsSchema(BaseSchema):
 
 
 class CallFilterSurrogatesSchema(BaseSchema):
-    user = fields.Nested('UserSchema',
-                         only=['uuid', 'firstname', 'lastname', 'links'],
-                         dump_only=True)
+    user = fields.Nested(
+        'UserSchema',
+        only=['uuid', 'firstname', 'lastname', 'links'],
+        dump_only=True,
+    )
 
     @post_dump
     def merge_user(self, data):
