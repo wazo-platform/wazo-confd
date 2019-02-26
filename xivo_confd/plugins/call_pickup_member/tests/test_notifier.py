594b2c80b4bc19ee3f425c0e5e0279e9de97b512
Francois Blackburn <fblackburn@wazo.community>
2019-02-26 14:43:30 -0500

    call_pickup_member: reload module pjsip and sccp
    some options are added to pjsip.conf and sccp.conf

--- xivo_confd/plugins/call_pickup_member/tests/test_notifier.py
+++ xivo_confd/plugins/call_pickup_member/tests/test_notifier.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import unittest
@@ -18,18 +18,25 @@ from xivo_dao.alchemy.groupfeatures import GroupFeatures as Group
 
 from ..notifier import CallPickupMemberNotifier
 
+SYSCONFD_HANDLERS = {
+    'ctibus': [],
+    'ipbx': ['module reload res_pjsip.so', 'module reload chan_sccp.so'],
+    'agentbus': [],
+}
+
 
 class TestCallPickupInterceptorUserNotifier(unittest.TestCase):
 
     def setUp(self):
         self.bus = Mock()
+        self.sysconfd = Mock()
         self.user1 = Mock(User, uuid='abcd-1234')
         self.user2 = Mock(User, uuid='efgh-5678')
         self.group1 = Mock(Group, uuid='abcd-1234')
         self.group2 = Mock(Group, uuid='efgh-5678')
         self.call_pickup = Mock(CallPickup, id=3)
 
-        self.notifier = CallPickupMemberNotifier(self.bus)
+        self.notifier = CallPickupMemberNotifier(self.bus, self.sysconfd)
 
     def test_interceptor_users_associate_then_bus_event(self):
         expected_event = CallPickupInterceptorUsersAssociatedEvent(
@@ -41,6 +48,11 @@ class TestCallPickupInterceptorUserNotifier(unittest.TestCase):
 
         self.bus.send_bus_event.assert_called_once_with(expected_event)
 
+    def test_interceptor_users_associate_then_sysconfd_event(self):
+        self.notifier.interceptor_users_associated(self.call_pickup, [self.user1, self.user2])
+
+        self.sysconfd.exec_request_handlers.assert_called_once_with(SYSCONFD_HANDLERS)
+
     def test_target_users_associate_then_bus_event(self):
         expected_event = CallPickupTargetUsersAssociatedEvent(
             self.call_pickup.id,
@@ -50,6 +62,11 @@ class TestCallPickupInterceptorUserNotifier(unittest.TestCase):
         self.notifier.target_users_associated(self.call_pickup, [self.user1, self.user2])
 
         self.bus.send_bus_event.assert_called_once_with(expected_event)
+
+    def test_target_users_associate_then_sysconfd_event(self):
+        self.notifier.target_users_associated(self.call_pickup, [self.user1, self.user2])
+
+        self.sysconfd.exec_request_handlers.assert_called_once_with(SYSCONFD_HANDLERS)
 
     def test_interceptor_groups_associate_then_bus_event(self):
         expected_event = CallPickupInterceptorGroupsAssociatedEvent(
@@ -61,6 +78,11 @@ class TestCallPickupInterceptorUserNotifier(unittest.TestCase):
 
         self.bus.send_bus_event.assert_called_once_with(expected_event)
 
+    def test_interceptor_groups_associate_then_sysconfd_event(self):
+        self.notifier.interceptor_groups_associated(self.call_pickup, [self.group1, self.group2])
+
+        self.sysconfd.exec_request_handlers.assert_called_once_with(SYSCONFD_HANDLERS)
+
     def test_target_groups_associate_then_bus_event(self):
         expected_event = CallPickupTargetGroupsAssociatedEvent(
             self.call_pickup.id,
@@ -70,3 +92,8 @@ class TestCallPickupInterceptorUserNotifier(unittest.TestCase):
         self.notifier.target_groups_associated(self.call_pickup, [self.group1, self.group2])
 
         self.bus.send_bus_event.assert_called_once_with(expected_event)
+
+    def test_target_groups_associate_then_sysconfd_event(self):
+        self.notifier.target_groups_associated(self.call_pickup, [self.group1, self.group2])
+
+        self.sysconfd.exec_request_handlers.assert_called_once_with(SYSCONFD_HANDLERS)
