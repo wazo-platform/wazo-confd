e4082754cf4ac598cc7e6ca9bbe0900934502e97
Alexandre Fournier <afournier@wazo.io>
2019-03-25 14:58:29 -0400

    queue_fallback: add tenant_uuid

--- xivo_confd/plugins/queue_fallback/resource.py
+++ xivo_confd/plugins/queue_fallback/resource.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from flask import request
@@ -13,6 +13,7 @@ from .schema import QueueFallbackSchema
 class QueueFallbackList(ConfdResource):
 
     schema = QueueFallbackSchema
+    has_tenant_uuid = True
 
     def __init__(self, service, queue_dao):
         super(QueueFallbackList, self).__init__()
@@ -21,12 +22,14 @@ class QueueFallbackList(ConfdResource):
 
     @required_acl('confd.queues.{queue_id}.fallbacks.read')
     def get(self, queue_id):
-        queue = self.queue_dao.get(queue_id)
+        tenant_uuids = self._build_tenant_list({'recurse': True})
+        queue = self.queue_dao.get(queue_id, tenant_uuids=tenant_uuids)
         return self.schema().dump(queue.fallbacks).data
 
     @required_acl('confd.queues.{queue_id}.fallbacks.update')
     def put(self, queue_id):
-        queue = self.queue_dao.get(queue_id)
+        tenant_uuids = self._build_tenant_list({'recurse': True})
+        queue = self.queue_dao.get(queue_id, tenant_uuids=tenant_uuids)
         fallbacks = self.schema().load(request.get_json()).data
         self.service.edit(queue, fallbacks)
         return '', 204
