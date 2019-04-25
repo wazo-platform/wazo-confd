c89f7e69b72c7b039ef302958b0fc7daa3c6a120
Alexandre Fournier <afournier@wazo.io>
2019-04-25 16:13:35 -0400

    service_discovery: only use https scheme for request

--- xivo_confd/service_discovery.py
+++ xivo_confd/service_discovery.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import requests
@@ -9,16 +9,9 @@ from xivo_confd.config import API_VERSION
 
 # this function is not executed from the main thread
 def self_check(config):
-    if config['rest_api']['http']['enabled']:
-        scheme = 'http'
-        port = config['rest_api']['http']['port']
-    elif config['rest_api']['https']['enabled']:
-        scheme = 'https'
-        port = config['rest_api']['https']['port']
-    else:
-        return False
+    port = config['rest_api']['port']
 
-    url = '{}://{}:{}/{}/infos'.format(scheme, 'localhost', port, API_VERSION)
+    url = '{}://{}:{}/{}/infos'.format('https', 'localhost', port, API_VERSION)
     try:
         return requests.get(url, headers={'accept': 'application/json'}, verify=False).status_code in (200, 401)
     except Exception:
