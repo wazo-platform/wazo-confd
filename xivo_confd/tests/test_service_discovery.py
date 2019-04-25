02c96b7b90dc536086bd87368ffc06e438fd8d88
Alexandre Fournier <afournier@wazo.io>
2019-04-25 16:59:47 -0400

    tests: fix service discovery test

--- xivo_confd/tests/test_service_discovery.py
+++ xivo_confd/tests/test_service_discovery.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016 Avencall
+# Copyright 2016-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import unittest
@@ -13,13 +13,9 @@ from ..service_discovery import self_check
 class TestSelfCheck(unittest.TestCase):
 
     def setUp(self):
-        self.http_port = 4242
         self.https_port = 4243
-        self.config = {'rest_api': {'http': {'enabled': True,
-                                             'port': self.http_port},
-                                    'https': {'enabled': True,
-                                              'port': self.https_port,
-                                              'certificate': 'my-certificate'}}}
+        self.config = {'rest_api': {'port': self.https_port,
+                                    'certificate': 'my-certificate'}}
 
     @patch('xivo_confd.service_discovery.requests.get', return_value=Mock(status_code=404))
     def test_that_self_check_returns_false_if_infos_does_not_return_200(self, get):
@@ -27,7 +23,7 @@ class TestSelfCheck(unittest.TestCase):
 
         assert_that(result, equal_to(False))
 
-        self.assert_get_called(get, 'http://localhost:4242/1.1/infos')
+        self.assert_get_called(get, 'https://localhost:4243/1.1/infos')
 
     @patch('xivo_confd.service_discovery.requests.get', return_value=Mock(status_code=200))
     def test_that_self_check_returns_true_if_infos_returns_200(self, get):
@@ -35,19 +31,10 @@ class TestSelfCheck(unittest.TestCase):
 
         assert_that(result, equal_to(True))
 
-        self.assert_get_called(get, 'http://localhost:4242/1.1/infos')
+        self.assert_get_called(get, 'https://localhost:4243/1.1/infos')
 
     @patch('xivo_confd.service_discovery.requests.get', return_value=Mock(status_code=401))
     def test_that_self_check_returns_true_if_infos_returns_401(self, get):
-        result = self_check(self.config)
-
-        assert_that(result, equal_to(True))
-
-        self.assert_get_called(get, 'http://localhost:4242/1.1/infos')
-
-    @patch('xivo_confd.service_discovery.requests.get', return_value=Mock(status_code=200))
-    def test_that_https_is_used_if_http_is_not_enabled(self, get):
-        self.config['rest_api']['http']['enabled'] = False
         result = self_check(self.config)
 
         assert_that(result, equal_to(True))
@@ -60,7 +47,7 @@ class TestSelfCheck(unittest.TestCase):
 
         assert_that(result, equal_to(False))
 
-        self.assert_get_called(get, 'http://localhost:4242/1.1/infos')
+        self.assert_get_called(get, 'https://localhost:4243/1.1/infos')
 
     def assert_get_called(self, get, url):
         get.assert_called_once_with(url, headers={'accept': 'application/json'}, verify=ANY)
