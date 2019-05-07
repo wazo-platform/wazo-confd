591fbe9aa7dea668a325ea34d840c1ec129f3ca2
Alexandre Fournier <afournier@wazo.io>
2019-05-07 18:00:10 -0400

    skill_rule: remove unique name and regex validators

--- xivo_confd/plugins/skill_rule/schema.py
+++ xivo_confd/plugins/skill_rule/schema.py
@@ -3,17 +3,15 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from marshmallow import fields, post_load, pre_dump
-from marshmallow.validate import Length, Regexp
+from marshmallow.validate import Length
 
 from xivo_confd.helpers.mallow import BaseSchema, Link, ListLink
-
-NAME_REGEX = r'^[-_.a-zA-Z0-9]+$'
 
 
 class SkillRuleSchema(BaseSchema):
     id = fields.Integer(dump_only=True)
     tenant_uuid = fields.String(dump_only=True)
-    name = fields.String(validate=(Regexp(NAME_REGEX), Length(max=64)), required=True)
+    name = fields.String(validate=(Length(max=64)), required=True)
     rules = fields.Nested('SkillRuleRuleSchema', many=True, allow_none=True)
     links = ListLink(Link('skillrules'))
 
