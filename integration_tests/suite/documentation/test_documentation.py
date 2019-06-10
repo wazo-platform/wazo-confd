61b7998876d7ab3f4817943d6cfdf30d057cbc71
Francois Blackburn <fblackburn@wazo.community>
2019-06-10 15:13:34 -0400

    use openapi-spec-validator

--- integration_tests/suite/documentation/test_documentation.py
+++ integration_tests/suite/documentation/test_documentation.py
@@ -1,14 +1,18 @@
 # Copyright 2016-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import logging
 import requests
-import pprint
+import yaml
 
-from hamcrest import assert_that, empty
+from openapi_spec_validator import validate_v2_spec
 
 from ..helpers.base import IntegrationTest
 
 requests.packages.urllib3.disable_warnings()
+
+logger = logging.getLogger('openapi_spec_validator')
+logger.setLevel(logging.INFO)
 
 
 class TestDocumentation(IntegrationTest):
@@ -19,10 +23,4 @@ class TestDocumentation(IntegrationTest):
         confd_port = self.service_port(9486, 'confd')
         api_url = 'https://localhost:{port}/1.1/api/api.yml'.format(port=confd_port)
         api = requests.get(api_url, verify=False)
-        self.validate_api(api)
-
-    def validate_api(self, api):
-        validator_port = self.service_port(8080, 'swagger-validator')
-        validator_url = 'http://localhost:{port}/debug'.format(port=validator_port)
-        response = requests.post(validator_url, data=api)
-        assert_that(response.json(), empty(), pprint.pformat(response.json()))
+        validate_v2_spec(yaml.safe_load(api.text))
