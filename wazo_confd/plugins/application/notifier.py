cd9c0fbc06f34491780ff9d861b242154bbcdc41
Francois Blackburn <fblackburn@wazo.community>
2019-06-28 14:04:06 -0400

    application: add body to events

--- wazo_confd/plugins/application/notifier.py
+++ wazo_confd/plugins/application/notifier.py
@@ -9,6 +9,16 @@ from xivo_bus.resources.application.event import (
 
 from wazo_confd import bus
 
+from .schema import ApplicationSchema
+
+APPLICATION_FIELDS = [
+    'uuid',
+    'tenant_uuid',
+    'name',
+    'destination',
+    'destination_options',
+]
+
 
 class ApplicationNotifier:
 
@@ -16,15 +26,18 @@ class ApplicationNotifier:
         self.bus = bus
 
     def created(self, application):
-        event = CreateApplicationEvent(application.uuid)
+        app_serialized = ApplicationSchema(only=APPLICATION_FIELDS).dump(application).data
+        event = CreateApplicationEvent(app_serialized)
         self.bus.send_bus_event(event)
 
     def edited(self, application):
-        event = EditApplicationEvent(application.uuid)
+        app_serialized = ApplicationSchema(only=APPLICATION_FIELDS).dump(application).data
+        event = EditApplicationEvent(app_serialized)
         self.bus.send_bus_event(event)
 
     def deleted(self, application):
-        event = DeleteApplicationEvent(application.uuid)
+        app_serialized = ApplicationSchema(only=APPLICATION_FIELDS).dump(application).data
+        event = DeleteApplicationEvent(app_serialized)
         self.bus.send_bus_event(event)
 
 
