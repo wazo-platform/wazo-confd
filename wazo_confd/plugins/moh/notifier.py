9120643dbdfc84293f91cb29eb7e216a058c1c46
Francois Blackburn <fblackburn@wazo.community>
2019-07-04 08:06:20 -0400

    moh: fix events import

--- wazo_confd/plugins/moh/notifier.py
+++ wazo_confd/plugins/moh/notifier.py
@@ -1,7 +1,7 @@
 # Copyright 2017-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from xivo_bus.resources.application.event import (
+from xivo_bus.resources.moh.event import (
     CreateMohEvent,
     DeleteMohEvent,
     EditMohEvent,
