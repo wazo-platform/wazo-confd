5772e3d5368b4a556b32c1053cd92ab9d3c194b9
Francois Blackburn <fblackburn@wazo.community>
2019-07-31 08:10:22 -0400

    registrar: fix copyright copy/paste

--- integration_tests/suite/helpers/helpers/registrar.py
+++ integration_tests/suite/helpers/helpers/registrar.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import string
--- wazo_confd/plugins/registrar/dao.py
+++ wazo_confd/plugins/registrar/dao.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_dao.helpers import errors
