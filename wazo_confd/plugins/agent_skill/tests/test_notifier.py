20c0df17827e495f8dba3f4476e7d59e4d8a8fdf
Francois Blackburn <fblackburn@wazo.community>
2019-11-16 13:07:29 -0500

    agent_skill: reload app-queue after di/association
    reason: asterisk need to reload queueskills.conf

--- wazo_confd/plugins/agent_skill/tests/test_notifier.py
+++ wazo_confd/plugins/agent_skill/tests/test_notifier.py
@@ -12,13 +12,21 @@ from xivo_bus.resources.agent_skill.event import (
 
 from ..notifier import AgentSkillNotifier
 
+EXPECTED_HANDLERS = {'ipbx': ['module reload app_queue.so'], 'agentbus': []}
+
 
 class TestAgentMemberNotifier(unittest.TestCase):
     def setUp(self):
         self.bus = Mock()
+        self.sysconfd = Mock()
         self.agent = Mock(id=1)
         self.agent_skill = Mock(skill=Mock(id=1))
-        self.notifier = AgentSkillNotifier(self.bus)
+        self.notifier = AgentSkillNotifier(self.bus, self.sysconfd)
+
+    def test_skill_associate_then_call_expected_handlers(self):
+        self.notifier.skill_associated(self.agent, self.agent_skill)
+
+        self.sysconfd.exec_request_handlers.assert_called_once_with(EXPECTED_HANDLERS)
 
     def test_skill_associate_then_bus_event(self):
         expected_event = AgentSkillAssociatedEvent(
@@ -28,6 +36,11 @@ class TestAgentMemberNotifier(unittest.TestCase):
         self.notifier.skill_associated(self.agent, self.agent_skill)
 
         self.bus.send_bus_event.assert_called_once_with(expected_event)
+
+    def test_skill_dissociate_then_call_expected_handlers(self):
+        self.notifier.skill_dissociated(self.agent, self.agent_skill)
+
+        self.sysconfd.exec_request_handlers.assert_called_once_with(EXPECTED_HANDLERS)
 
     def test_skill_dissociate_then_bus_event(self):
         expected_event = AgentSkillDissociatedEvent(
