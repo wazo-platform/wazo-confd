f665f106e5631d487755b841052704bdf1522c8a
Sylvain Afchain <safchain@gmail.com>
2020-01-20 10:54:17 +0100

    Make ssl optional

--- wazo_confd/service_discovery.py
+++ wazo_confd/service_discovery.py
@@ -8,9 +8,11 @@ from wazo_confd.config import API_VERSION
 
 # this function is not executed from the main thread
 def self_check(config):
-    port = config['rest_api']['port']
-
-    url = '{}://{}:{}/{}/infos'.format('https', 'localhost', port, API_VERSION)
+    port = config["rest_api"]["port"]
+    scheme = "http"
+    if config["rest_api"]["certificate"] and config["rest_api"]["private_key"]:
+        scheme = "https"
+    url = "{}://{}:{}/{}/infos".format(scheme, "localhost", port, API_VERSION)
     try:
         return requests.get(
             url, headers={'accept': 'application/json'}, verify=False
