3246b98dffeb079b2988be5770ca5a6ce7df8347
Pascal Cadotte Michaud <pcm@wazo.io>
2020-04-01 07:33:13 -0400

    transports: refactor notifier to remove some duplication

--- wazo_confd/plugins/pjsip/notifier.py
+++ wazo_confd/plugins/pjsip/notifier.py
@@ -45,27 +45,24 @@ class PJSIPTransportNotifier:
         self.sysconfd = sysconfd
         self.schema = schema
 
-    def send_sysconfd_handlers(self, ipbx_command):
-        handlers = {'ipbx': [ipbx_command]}
+    def send_sysconfd_handlers(self):
+        handlers = {'ipbx': ['module reload res_pjsip.so']}
         self.sysconfd.exec_request_handlers(handlers)
 
     def created(self, transport):
-        ipbx_command = 'module reload res_pjsip.so'
-        self.send_sysconfd_handlers(ipbx_command)
+        self.send_sysconfd_handlers()
         body = self.schema.dump(transport)
         event = CreateSIPTransportEvent(body)
         self.bus.send_bus_event(event)
 
     def deleted(self, transport):
-        ipbx_command = 'module reload res_pjsip.so'
-        self.send_sysconfd_handlers(ipbx_command)
+        self.send_sysconfd_handlers()
         body = self.schema.dump(transport)
         event = DeleteSIPTransportEvent(body)
         self.bus.send_bus_event(event)
 
     def edited(self, transport):
-        ipbx_command = 'module reload res_pjsip.so'
-        self.send_sysconfd_handlers(ipbx_command)
+        self.send_sysconfd_handlers()
         body = self.schema.dump(transport)
         event = EditSIPTransportEvent(body)
         self.bus.send_bus_event(event)
