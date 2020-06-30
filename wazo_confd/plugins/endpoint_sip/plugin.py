7c528403c8361e2e93f38fcc0e1ccc7fbcb5fcb6
Pascal Cadotte Michaud <pcm@wazo.io>
2020-06-30 15:19:20 -0400

    pjsip: add a separate route for configuration templates
    create a separate resource /endpoints/sip/templates for the SIP configuration
    templates instead of having a template field on a SIP endpoint.
    
    This will allow the UI to avoid filtering templates and "real" endpoints

--- wazo_confd/plugins/endpoint_sip/plugin.py
+++ wazo_confd/plugins/endpoint_sip/plugin.py
@@ -6,8 +6,8 @@ from wazo_provd_client import Client as ProvdClient
 from xivo_dao.resources.endpoint_sip import dao as sip_dao
 from xivo_dao.resources.pjsip_transport import dao as transport_dao
 
-from .resource import SipItem, SipList
-from .service import build_service
+from .resource import SipItem, SipList, SipTemplateItem, SipTemplateList
+from .service import build_endpoint_service, build_template_service
 
 
 class Plugin:
@@ -19,16 +19,28 @@ class Plugin:
         provd_client = ProvdClient(**config['provd'])
         token_changed_subscribe(provd_client.set_token)
 
-        service = build_service(provd_client)
+        endpoint_service = build_endpoint_service(provd_client)
+        template_service = build_template_service(provd_client)
 
         api.add_resource(
             SipItem,
             '/endpoints/sip/<uuid:uuid>',
             endpoint='endpoint_sip',
-            resource_class_args=(service, transport_dao),
+            resource_class_args=(endpoint_service, transport_dao),
         )
         api.add_resource(
             SipList,
             '/endpoints/sip',
-            resource_class_args=(service, sip_dao, transport_dao)
+            resource_class_args=(endpoint_service, sip_dao, transport_dao),
+        )
+        api.add_resource(
+            SipTemplateItem,
+            '/endpoints/sip/templates/<uuid:uuid>',
+            endpoint='endpoint_sip_templates',
+            resource_class_args=(template_service, transport_dao),
+        )
+        api.add_resource(
+            SipTemplateList,
+            '/endpoints/sip/templates',
+            resource_class_args=(template_service, sip_dao, transport_dao),
         )
