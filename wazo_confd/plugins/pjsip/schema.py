b250a86a0fe4d7ef020a2d584adea644624145f4
Francois Blackburn <fblackburn@wazo.community>
2020-06-30 13:41:03 -0400

    blackify

--- wazo_confd/plugins/pjsip/schema.py
+++ wazo_confd/plugins/pjsip/schema.py
@@ -13,8 +13,4 @@ class PJSIPTransportDeleteRequestSchema(BaseSchema):
 class PJSIPTransportSchema(BaseSchema):
     uuid = fields.UUID(dump_only=True)
     name = fields.String(validate=PJSIPSection(), required=True)
-    options = fields.List(
-        PJSIPSectionOption(),
-        validate=Length(max=128),
-        missing=[],
-    )
+    options = fields.List(PJSIPSectionOption(), validate=Length(max=128), missing=[],)
