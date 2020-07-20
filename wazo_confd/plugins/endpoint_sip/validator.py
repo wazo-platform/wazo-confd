54ec9d9508be4cc07724defd84a57e800c47c088
Francois Blackburn <fblackburn@wazo.community>
2020-07-20 13:26:40 -0400

    sip/template/pjsip: test that name doesn't conflict

--- wazo_confd/plugins/endpoint_sip/validator.py
+++ wazo_confd/plugins/endpoint_sip/validator.py
@@ -1,8 +1,10 @@
 # Copyright 2015-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from functools import partial
 
-from xivo_dao.resources.endpoint_sip import dao as sip_dao
+from xivo_dao.resources.endpoint_sip import dao as sip_dao_module
+from xivo_dao.resources.pjsip_transport import dao as transport_dao
 
 from wazo_confd.helpers.validator import (
     Optional,
@@ -13,17 +15,23 @@ from wazo_confd.helpers.validator import (
 
 
 def build_validator():
+    sip_find_by = partial(sip_dao_module.find_by, template=False)
+    template_find_by = partial(sip_dao_module.find_by, template=True)
     return ValidationGroup(
         create=[
             Optional(
                 'name',
-                UniqueField('name', lambda v: sip_dao.find_by(name=v), 'SIPEndpoint'),
+                UniqueField('name', lambda v: sip_find_by(name=v), 'SIPEndpoint'),
+                UniqueField('name', lambda v: template_find_by(name=v), 'SIPEndpointTemplate'),
+                UniqueField('name', lambda v: transport_dao.find_by(name=v), 'Transport'),
             ),
         ],
         edit=[
             Optional(
                 'name',
-                UniqueFieldChanged('name', sip_dao.find_by, 'SIPEndpoint', id_field='uuid'),
+                UniqueFieldChanged('name', sip_find_by, 'SIPEndpoint', id_field='uuid'),
+                UniqueFieldChanged('name', template_find_by, 'SIPEndpointTemplate', id_field='uuid'),
+                UniqueFieldChanged('name', transport_dao.find_by, 'Transport', id_field='uuid'),
             )
         ],
     )
