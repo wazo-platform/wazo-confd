1e55376b26c14bb06ec8903e51badba1aae6bfbe
Francois Blackburn <fblackburn@wazo.community>
2020-08-11 09:24:25 -0400

    extract PJSIPDoc/Validator to helpers

--- wazo_confd/plugins/pjsip/validator.py
+++ wazo_confd/plugins/pjsip/validator.py
@@ -13,26 +13,7 @@ from wazo_confd.helpers.validator import (
     Validator,
     ValidationGroup,
 )
-
-
-class PJSIPDocValidator(Validator):
-    def __init__(self, field, section, pjsip_doc):
-        self.field = field
-        self.pjsip_doc = pjsip_doc
-        self.section = section
-
-    def validate(self, model):
-        self._validate(model)
-
-    def _validate(self, model):
-        values = getattr(model, self.field, [])
-        option_names = [value[0] for value in values]
-        for option_name in option_names:
-            if not self.pjsip_doc.is_valid_in_section(self.section, option_name):
-                raise errors.invalid_choice(
-                    field='{}: invalid variable ({})'.format(self.field, option_name),
-                    choices=self.pjsip_doc.get_section_variables(self.section),
-                )
+from wazo_confd.helpers.asterisk import PJSIPDocValidator
 
 
 class TransportDeleteValidator(Validator):
