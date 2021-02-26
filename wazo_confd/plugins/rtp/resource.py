05fa55c56d98af388d8799412bfe9e5c2160b0da
Francois Blackburn <fblackburn@wazo.community>
2021-02-26 11:43:27 -0500

    restrict rtp to master tenant

--- wazo_confd/plugins/rtp/resource.py
+++ wazo_confd/plugins/rtp/resource.py
@@ -1,17 +1,19 @@
-# Copyright 2018-2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from wazo_confd.auth import required_acl
+from wazo_confd.auth import required_acl, required_master_tenant
 from wazo_confd.helpers.asterisk import AsteriskConfigurationList
 
 
 class RTPGeneralList(AsteriskConfigurationList):
     section_name = 'general'
 
+    @required_master_tenant()
     @required_acl('confd.asterisk.rtp.general.get')
     def get(self):
         return super().get()
 
+    @required_master_tenant()
     @required_acl('confd.asterisk.rtp.general.update')
     def put(self):
         return super().put()
@@ -20,10 +22,12 @@ class RTPGeneralList(AsteriskConfigurationList):
 class RTPIceHostCandidatesList(AsteriskConfigurationList):
     section_name = 'ice_host_candidates'
 
+    @required_master_tenant()
     @required_acl('confd.asterisk.rtp.ice_host_candidates.get')
     def get(self):
         return super().get()
 
+    @required_master_tenant()
     @required_acl('confd.asterisk.rtp.ice_host_candidates.update')
     def put(self):
         return super().put()
