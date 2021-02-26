26c1458384dd13e0b5970b37f856160ebf2657fd
Francois Blackburn <fblackburn@wazo.community>
2021-02-26 10:57:10 -0500

    restrict confbridge to master tenant

--- wazo_confd/plugins/confbridge/resource.py
+++ wazo_confd/plugins/confbridge/resource.py
@@ -1,17 +1,19 @@
-# Copyright 2017-2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from wazo_confd.auth import required_acl
+from wazo_confd.auth import required_acl, required_master_tenant
 from wazo_confd.helpers.asterisk import AsteriskConfigurationList
 
 
 class ConfBridgeWazoDefaultBridgeList(AsteriskConfigurationList):
     section_name = 'wazo_default_bridge'
 
+    @required_master_tenant()
     @required_acl('confd.asterisk.confbridge.wazo_default_bridge.get')
     def get(self):
         return super().get()
 
+    @required_master_tenant()
     @required_acl('confd.asterisk.confbridge.wazo_default_bridge.update')
     def put(self):
         return super().put()
@@ -20,10 +22,12 @@ class ConfBridgeWazoDefaultBridgeList(AsteriskConfigurationList):
 class ConfBridgeWazoDefaultUserList(AsteriskConfigurationList):
     section_name = 'wazo_default_user'
 
+    @required_master_tenant()
     @required_acl('confd.asterisk.confbridge.wazo_default_user.get')
     def get(self):
         return super().get()
 
+    @required_master_tenant()
     @required_acl('confd.asterisk.confbridge.wazo_default_user.update')
     def put(self):
         return super().put()
