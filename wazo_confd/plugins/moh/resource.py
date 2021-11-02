3ecaff9453934b00a1da98e0a133929b87d43d90
Alexandre Fournier <afournier@wazo.io>
2021-11-02 15:29:53 -0400

    moh: format name using slug

--- wazo_confd/plugins/moh/resource.py
+++ wazo_confd/plugins/moh/resource.py
@@ -1,7 +1,8 @@
-# Copyright 2017-2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from flask import request, url_for
+from uuid import uuid4
 
 from xivo_dao.alchemy.moh import MOH
 
@@ -15,13 +16,30 @@ class MohList(ListResource):
 
     model = MOH
     schema = MohSchema
+    moh_name_fmt = 'moh-{tenant_slug}-{moh_uuid}'
+
+    def __init__(self, tenant_dao, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._tenant_dao = tenant_dao
 
     def build_headers(self, moh):
         return {'Location': url_for('moh', uuid=moh.uuid, _external=True)}
 
     @required_acl('confd.moh.create')
     def post(self):
-        return super().post()
+        form = self.schema().load(request.get_json())
+        form = self.add_tenant_to_form(form)
+
+        tenant = self._tenant_dao.get(form['tenant_uuid'])
+        form['uuid'] = uuid4()
+        form['name'] = self.moh_name_fmt.format(
+            tenant_slug=tenant.slug,
+            moh_uuid=form['uuid'],
+        )
+
+        model = self.model(**form)
+        model = self.service.create(model)
+        return self.schema().dump(model), 201, self.build_headers(model)
 
     @required_acl('confd.moh.read')
     def get(self):
