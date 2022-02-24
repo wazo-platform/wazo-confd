d1b448c690abdd1d65d0432e2697d48698b0c430
Francois Blackburn <fblackburn@wazo.community>
2022-02-24 10:22:43 -0500

    remove unnecessary unknown for inherited Nested field
    why: see WAZO-1729

--- wazo_confd/plugins/call_filter_fallback/schema.py
+++ wazo_confd/plugins/call_filter_fallback/schema.py
@@ -1,7 +1,7 @@
 # Copyright 2018-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from marshmallow import EXCLUDE, post_load
+from marshmallow import post_load
 
 from xivo_dao.alchemy.dialaction import Dialaction
 from wazo_confd.helpers.mallow import BaseSchema
@@ -10,7 +10,7 @@ from wazo_confd.helpers.destination import DestinationField
 
 class CallFilterFallbackSchema(BaseSchema):
     noanswer_destination = DestinationField(
-        attribute='noanswer', default=None, allow_none=True, unknown=EXCLUDE
+        attribute='noanswer', default=None, allow_none=True
     )
 
     @post_load
