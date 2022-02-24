59b5fb71ffa8fe29b612cce871f4aae95f49aa7c
Francois Blackburn <fblackburn@wazo.community>
2022-02-24 10:15:57 -0500

    remove unused logger import

--- wazo_confd/helpers/mallow.py
+++ wazo_confd/helpers/mallow.py
@@ -2,14 +2,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import re
-import logging
 
 from flask import url_for
 from flask_restful import abort
 from marshmallow import EXCLUDE, Schema, fields, pre_load, validate
 from marshmallow.exceptions import RegistryError, ValidationError
-
-logger = logging.getLogger(__name__)
 
 
 class BaseSchema(Schema):
