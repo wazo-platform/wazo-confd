b81a3354a9fdd78bae616c5705dd4203f6e5096c
Nabil Ben Youssef <nabil.benyoussef@wazo.io>
2022-08-26 12:27:49 +0200

    Add new endpoint to manage easily a user and his associated resources

---
+++ integration_tests/suite/base/test_unified_users.py
@@ -0,0 +1,108 @@
+# Copyright 2022 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from hamcrest import (
+    all_of,
+    assert_that,
+    contains,
+    contains_inanyorder,
+    empty,
+    equal_to,
+    has_entries,
+    has_entry,
+    has_item,
+    has_items,
+    is_not,
+    none,
+    not_,
+)
+
+from . import confd
+
+FULL_USER = {
+    "firstname": "Jôhn",
+    "lastname": "Smêth",
+}
+
+NULL_USER = {
+    "firstname": "Jôhn",
+    "lastname": None,
+}
+
+ALL_NULL_USER = {
+    "firstname": None,
+    "lastname": None,
+}
+
+
+def test_post_basic_user_no_error():
+    response = confd.unified_users.post({'user': {'firstname': 'Jôhn'}}).response
+    assert response.status_code == 201
+
+    returned_json = response.json()
+    assert 'user' in returned_json
+
+    created_user = returned_json['user']
+    assert created_user['firstname'] == 'Jôhn'
+    assert 'id' in created_user and created_user['id']
+    assert 'uuid' in created_user and created_user['uuid']
+
+
+def test_post_user_missing_field_return_error():
+    response = confd.unified_users.post({'user': {'lastname': 'Jôhn'}}).response
+    assert response.status_code == 400
+    assert response.json()['details']['user']['firstname']['constraint_id'] == 'required'
+
+
+def test_post_user_wrong_type_return_error():
+    user = {
+        "firstname": "Rîchard",
+        "enabled": "True"
+    }
+    response = confd.unified_users.post({'user': user}).response
+    assert response.status_code == 400
+    assert (
+            response.json()['details']['user']['enabled']['constraint_id'] == 'type' and
+            response.json()['details']['user']['enabled']['constraint'] == 'boolean')
+
+
+def test_post_full_user_no_error():
+    user = {
+        "subscription_type": 2,
+        "firstname": "Rîchard",
+        "lastname": "Lâpointe",
+        "email": "richard@lapointe.org",
+        "language": "fr_FR",
+        "outgoing_caller_id": '"Rîchy Cool" <4185551234>',
+        "mobile_phone_number": "4181234567",
+        "supervision_enabled": True,
+        "call_transfer_enabled": False,
+        "dtmf_hangup_enabled": True,
+        "call_record_outgoing_external_enabled": True,
+        "call_record_outgoing_internal_enabled": True,
+        "call_record_incoming_internal_enabled": True,
+        "call_record_incoming_external_enabled": True,
+        "online_call_record_enabled": False,
+        "simultaneous_calls": 5,
+        "ring_seconds": 30,
+        "userfield": "userfield",
+        "call_permission_password": "1234",
+        "enabled": True,
+        "username": "richardlapointe",
+        "password": "secret",
+    }
+    response = confd.unified_users.post({'user': user}).response
+
+    assert response.status_code == 201
+
+    returned_json = response.json()
+
+    assert 'user' in returned_json
+
+    created_user = returned_json['user']
+
+    assert_that(
+        created_user,
+        has_entries(user)
+    )
+    assert 'uuid' in created_user and created_user['uuid']
--- wazo_confd/helpers/restful.py
+++ wazo_confd/helpers/restful.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from flask import request
@@ -72,9 +72,10 @@ class ConfdResource(ErrorCatchingResource):
 
 
 class ListResource(ConfdResource):
-    def __init__(self, service):
+    def __init__(self, service, json_path=None):
         super().__init__()
         self.service = service
+        self.json_path=json_path
 
     def get(self):
         params = self.search_params()
@@ -100,7 +101,9 @@ class ListResource(ConfdResource):
         return form
 
     def post(self):
-        form = self.schema().load(request.get_json())
+        form = self.schema().load(
+            find(self.json_path, request.get_json())
+        )
         form = self.add_tenant_to_form(form)
         model = self.model(**form)
         model = self.service.create(model)
@@ -158,3 +161,13 @@ class ItemResource(ConfdResource):
 
         tenant_uuids = self._build_tenant_list({'recurse': True})
         return {'tenant_uuids': tenant_uuids}
+
+
+def find(path, j):
+    if not path:
+        return j
+    keys = path.split('.')
+    rv = j
+    for key in keys:
+        rv = rv[key]
+    return rv
