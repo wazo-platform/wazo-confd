808a20b8444913b3fe3e31f720b010634d521ef4
Pascal Cadotte Michaud <pcm@wazo.io>
2022-08-30 08:56:54 -0400

    HTTP: introduce multiple versions of the API
    at the moment only version 1.1 is used. I kept the `api` key in the dependencies
    dictionnary to avoid breaking community plugins

--- wazo_confd/plugins/user/plugin.py
+++ wazo_confd/plugins/user/plugin.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_provd_client import Client as ProvdClient
@@ -18,7 +18,7 @@ from .service import build_service, build_service_callservice, build_service_for
 
 class Plugin:
     def load(self, dependencies):
-        api = dependencies['api']
+        api_v1_1 = dependencies['api_v1_1']
         config = dependencies['config']
         token_changed_subscribe = dependencies['token_changed_subscribe']
 
@@ -29,7 +29,7 @@ class Plugin:
         service_callservice = build_service_callservice()
         service_forward = build_service_forward()
 
-        api.add_resource(
+        api_v1_1.add_resource(
             UserItem,
             '/users/<uuid:id>',
             '/users/<int:id>',
@@ -37,53 +37,53 @@ class Plugin:
             resource_class_args=(service,),
         )
 
-        api.add_resource(
+        api_v1_1.add_resource(
             UserList, '/users', endpoint='users_list', resource_class_args=(service,)
         )
 
-        api.add_resource(
+        api_v1_1.add_resource(
             UserServiceDND,
             '/users/<uuid:user_id>/services/dnd',
             '/users/<int:user_id>/services/dnd',
             resource_class_args=(service_callservice,),
         )
 
-        api.add_resource(
+        api_v1_1.add_resource(
             UserServiceIncallFilter,
             '/users/<uuid:user_id>/services/incallfilter',
             '/users/<int:user_id>/services/incallfilter',
             resource_class_args=(service_callservice,),
         )
 
-        api.add_resource(
+        api_v1_1.add_resource(
             UserServiceList,
             '/users/<uuid:user_id>/services',
             '/users/<int:user_id>/services',
             resource_class_args=(service_callservice,),
         )
 
-        api.add_resource(
+        api_v1_1.add_resource(
             UserForwardBusy,
             '/users/<uuid:user_id>/forwards/busy',
             '/users/<int:user_id>/forwards/busy',
             resource_class_args=(service_forward,),
         )
 
-        api.add_resource(
+        api_v1_1.add_resource(
             UserForwardNoAnswer,
             '/users/<uuid:user_id>/forwards/noanswer',
             '/users/<int:user_id>/forwards/noanswer',
             resource_class_args=(service_forward,),
         )
 
-        api.add_resource(
+        api_v1_1.add_resource(
             UserForwardUnconditional,
             '/users/<uuid:user_id>/forwards/unconditional',
             '/users/<int:user_id>/forwards/unconditional',
             resource_class_args=(service_forward,),
         )
 
-        api.add_resource(
+        api_v1_1.add_resource(
             UserForwardList,
             '/users/<uuid:user_id>/forwards',
             '/users/<int:user_id>/forwards',
