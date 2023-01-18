e8d85eeb45260713a548b3a6f30d29b0f4b13d26
billou <nabil.benyoussef@wazo.io>
2023-01-18 20:05:51 +0100

    Update headers

--- integration_tests/suite/base/test_users.py
+++ integration_tests/suite/base/test_users.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from hamcrest import (
