4900a92202f8e99b9c962b618e63e7d93fb2506e
billou <nabil.benyoussef@wazo.io>
2023-02-09 21:03:04 +0100

    Update headers

--- wazo_confd/plugins/device/middleware.py
+++ wazo_confd/plugins/device/middleware.py
@@ -1,4 +1,4 @@
-# Copyright 2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_dao.helpers import errors
