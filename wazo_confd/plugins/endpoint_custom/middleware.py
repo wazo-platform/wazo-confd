63036e261bc60031c90dfda12aa54481657836b5
billou <nabil.benyoussef@wazo.io>
2023-02-17 14:35:34 +0100

    Remove duplicated code

--- wazo_confd/plugins/endpoint_custom/middleware.py
+++ wazo_confd/plugins/endpoint_custom/middleware.py
@@ -38,20 +38,3 @@ class EndpointCustomMiddleWare:
     def update(self, endpoint_custom_id, body, tenant_uuids):
         model = self._service.get(endpoint_custom_id, tenant_uuids=tenant_uuids)
         self.parse_and_update(model, body)
-        for name, value in form.items():
-            setattr(model, name, value)
-        self._service.edit(model, updated_fields=updated_fields, **kwargs)
-
-    def find_updated_fields(self, model, form):
-        updated_fields = []
-        for name, value in form.items():
-            try:
-                if getattr(model, name) != value:
-                    updated_fields.append(name)
-            except AttributeError:
-                pass
-        return updated_fields
-
-    def update(self, endpoint_custom_id, body, tenant_uuids):
-        model = self._service.get(endpoint_custom_id, tenant_uuids=tenant_uuids)
-        self.parse_and_update(model, body)
