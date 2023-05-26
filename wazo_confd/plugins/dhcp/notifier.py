ecf055140ddbaf970614fb97a891ff316fd5d27f
Charles Langlois <charles.langlois@wazo.io>
2023-05-26 17:16:13 -0400

    Added type annotations

--- wazo_confd/plugins/dhcp/notifier.py
+++ wazo_confd/plugins/dhcp/notifier.py
@@ -1,13 +1,15 @@
 # Copyright 2019-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from wazo_confd._bus import BusPublisher
+from wazo_confd._sysconfd import SysconfdPublisher
 from xivo_bus.resources.dhcp.event import DHCPEditedEvent
 
 
 class DHCPNotifier:
     def __init__(self, bus, sysconfd):
-        self.bus = bus
-        self.sysconfd = sysconfd
+        self.bus: BusPublisher = bus
+        self.sysconfd: SysconfdPublisher = sysconfd
 
     def edited(self):
         event = DHCPEditedEvent()
