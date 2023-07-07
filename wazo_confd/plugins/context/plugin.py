72be02548ca2274bc095d5419c80ed4b3bdb605c
Alexandre Fournier <afournier@wazo.io>
2023-07-07 11:32:11 -0400

    context: use autogenerated name

--- wazo_confd/plugins/context/plugin.py
+++ wazo_confd/plugins/context/plugin.py
@@ -1,5 +1,8 @@
 # Copyright 2016-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+from xivo_dao.resources.tenant import dao as tenant_dao
+
 from .middleware import ContextMiddleWare
 from .resource import ContextItem, ContextList
 from .service import build_service
@@ -14,7 +17,9 @@ class Plugin:
         context_middleware = ContextMiddleWare(service)
         middleware_handle.register('context', context_middleware)
 
-        api.add_resource(ContextList, '/contexts', resource_class_args=(service,))
+        api.add_resource(
+            ContextList, '/contexts', resource_class_args=(tenant_dao, service)
+        )
 
         api.add_resource(
             ContextItem,
