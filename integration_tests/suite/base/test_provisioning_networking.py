4ce46b7a568fb42481efddb141b9f3f265a2e286
Alexandre Fournier <afournier@wazo.io>
2023-09-15 11:08:49 -0400

    provisioning: add http_base_url

--- integration_tests/suite/base/test_provisioning_networking.py
+++ integration_tests/suite/base/test_provisioning_networking.py
@@ -1,9 +1,10 @@
-# Copyright 2019-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from hamcrest import assert_that, has_entries
 
 from . import confd
+from ..helpers import scenarios as s
 from ..helpers.config import TOKEN_SUB_TENANT
 
 
@@ -11,9 +12,26 @@ def test_get():
     response = confd.provisioning.networking.get()
     assert_that(
         response.item,
-        has_entries({'provision_host': '', 'provision_http_port': 8667}),
+        has_entries(
+            {
+                'provision_host': '',
+                'provision_http_port': 18667,
+                'provision_http_base_url': '',
+            }
+        ),
     )
 
+
+def test_put_errors():
+    url = confd.provisioning.networking.put
+
+    yield s.check_bogus_field_returns_error, url, 'provision_http_base_url', 123
+    yield s.check_bogus_field_returns_error, url, 'provision_http_base_url', True
+    yield s.check_bogus_field_returns_error, url, 'provision_http_base_url', {}
+    yield s.check_bogus_field_returns_error, url, 'provision_http_base_url', []
+    yield s.check_bogus_field_returns_error, url, 'provision_http_base_url', s.random_string(
+        256
+    )
 
 def test_put_minimal_parameters():
     body = {}
@@ -22,25 +40,46 @@ def test_put_minimal_parameters():
     response = confd.provisioning.networking.get()
     assert_that(
         response.item,
-        has_entries({'provision_host': '', 'provision_http_port': 8667}),
+        has_entries(
+            {
+                'provision_host': '',
+                'provision_http_port': 18667,
+                'provision_http_base_url': '',
+            }
+        ),
     )
 
-    body = {'provision_host': '127.0.0.1'}
+    body = {
+        'provision_host': '127.0.0.1',
+        'provision_http_base_url': 'http://example.com',
+    }
     result = confd.provisioning.networking.put(body)
     result.assert_status(204)
     response = confd.provisioning.networking.get()
     assert_that(
         response.item,
-        has_entries({'provision_host': '127.0.0.1', 'provision_http_port': 8667}),
+        has_entries(
+            {
+                'provision_host': '127.0.0.1',
+                'provision_http_port': 18667,
+                'provision_http_base_url': 'http://example.com',
+            }
+        ),
     )
 
-    body = {'provision_host': None}
+    body = {'provision_host': None, 'provision_http_base_url': None}
     result = confd.provisioning.networking.put(body)
     result.assert_status(204)
     response = confd.provisioning.networking.get()
     assert_that(
         response.item,
-        has_entries({'provision_host': None, 'provision_http_port': 8667}),
+        has_entries(
+            {
+                'provision_host': None,
+                'provision_http_port': 18667,
+                'provision_http_base_url': None,
+            }
+        ),
     )
 
 
@@ -48,6 +87,7 @@ def test_put_all_parameters():
     body = {
         'provision_host': '127.0.0.1',
         'provision_http_port': 8665,
+        'provision_http_base_url': 'http://example.com',
     }
     result = confd.provisioning.networking.put(body)
     result.assert_status(204)
--- wazo_confd/plugins/provisioning_networking/schema.py
+++ wazo_confd/plugins/provisioning_networking/schema.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from marshmallow import fields
+from marshmallow.validate import Length
 
 from wazo_confd.helpers.mallow import BaseSchema
 
@@ -9,3 +10,6 @@ from wazo_confd.helpers.mallow import BaseSchema
 class ProvisioningNetworkingSchema(BaseSchema):
     provision_host = fields.String(attribute='net4_ip', allow_none=True)
     provision_http_port = fields.Integer(attribute='http_port')
+    provision_http_base_url = fields.String(
+        validate=Length(max=255), attribute='http_base_url', allow_none=True
+    )
