350105edf4dca917fda973c81eaded3879e372bb
billou <nabil.benyoussef@wazo.io>
2023-10-25 15:39:26 +0200

    Remove unused category field in skill schema

--- wazo_confd/plugins/skill/schema.py
+++ wazo_confd/plugins/skill/schema.py
@@ -13,7 +13,6 @@ class SkillSchema(BaseSchema):
     id = fields.Integer(dump_only=True)
     tenant_uuid = fields.String(dump_only=True)
     name = fields.String(validate=(Regexp(NAME_REGEX), Length(max=64)), required=True)
-    category = fields.String(validate=Length(max=64), allow_none=True)
     description = fields.String(allow_none=True)
     links = ListLink(Link('skills'))
 
