8c6782c42ec3d042df85f24c437a8b0f525ba661
Francois Blackburn <fblackburn@wazo.community>
2024-07-19 06:42:06 -0400

    ingress http: fix wrong date from copy/paste

--- wazo_confd/plugins/ingress_http/plugin.py
+++ wazo_confd/plugins/ingress_http/plugin.py
@@ -1,4 +1,4 @@
-# Copyright 2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from .resource import IngressHTTPItem, IngressHTTPList
--- wazo_confd/plugins/ingress_http/resource.py
+++ wazo_confd/plugins/ingress_http/resource.py
@@ -1,4 +1,4 @@
-# Copyright 2021-2024 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from flask import request, url_for
