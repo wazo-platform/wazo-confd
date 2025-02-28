14ce6086ec844d6299785d9d7def6b0e3a1b8bd7
Alexandre Fournier <afournier@wazo.io>
2025-02-28 16:17:58 -0500

    recordings_announcements: reload res_pjsip on edit

--- wazo_confd/plugins/recordings/notifier.py
+++ wazo_confd/plugins/recordings/notifier.py
@@ -4,18 +4,28 @@
 from wazo_bus.resources.recordings.event import RecordingsAnnouncementsEditedEvent
 from wazo_confd.plugins.recordings.schema import RecordingAnnouncementSchema
 
-from wazo_confd import bus
+from wazo_confd import bus, sysconfd
 
 
 class RecordingAnnouncementNotifier:
-    def __init__(self, bus):
+    def __init__(self, bus, sysconfd):
         self.bus = bus
+        self.sysconfd = sysconfd
+
+    def send_sysconfd_handlers(self):
+        handlers = {
+            'ipbx': [
+                'module reload res_pjsip.so',
+            ],
+        }
+        self.sysconfd.exec_request_handlers(handlers)
 
     def edited(self, tenant):
         serialized = RecordingAnnouncementSchema().dump(tenant)
+        self.send_sysconfd_handlers()
         event = RecordingsAnnouncementsEditedEvent(serialized, tenant.uuid)
         self.bus.queue_event(event)
 
 
 def build_notifier():
-    return RecordingAnnouncementNotifier(bus)
+    return RecordingAnnouncementNotifier(bus, sysconfd)
