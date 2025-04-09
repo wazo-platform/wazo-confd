9f8faaf3bf70ccd0b6ae309b692ce4b56953bce5
Charles Langlois <charles.langlois@wazo.io>
2025-04-09 11:14:39 -0400

    blocklist: updated integration tests
    with multi-user setup and additional search parameters

--- integration_tests/suite/base/test_blocklist.py
+++ integration_tests/suite/base/test_blocklist.py
@@ -1,18 +1,12 @@
 # Copyright 2025 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from hamcrest import (
-    assert_that,
-    has_item,
-    has_items,
-    has_entries,
-    is_not,
-)
-from . import create_confd, confd
-from ..helpers import (
-    fixtures,
-    scenarios as s,
-)
+from hamcrest import assert_that, has_entries, has_item, has_items, is_not
+
+from ..helpers import fixtures
+from ..helpers import scenarios as s
+from ..helpers import utils
+from . import confd, create_confd
 
 FAKE_UUID = '99999999-9999-4999-9999-999999999999'
 
@@ -35,17 +29,19 @@ def test_get_errors():
 
 
 @fixtures.user()
-def test_search(user):
+@fixtures.user()
+def test_search(user, other_user):
     user_confd_client = create_confd(user_uuid=user['uuid'])
+    other_user_confd_client = create_confd(user_uuid=other_user['uuid'])
     numbers = [
         fixtures.users_blocklist_number(
             number='+18001235555', label='search', confd_client=user_confd_client
         ),
         fixtures.users_blocklist_number(
-            number='+15551112222', label='hidden', confd_client=user_confd_client
+            number='+15551112222', label='hidden', confd_client=other_user_confd_client
         ),
     ]
-    with numbers[0] as blocklist_number, numbers[1] as hidden:
+    with utils.context_group(*numbers) as (blocklist_number, hidden):
         url = confd.users.blocklist.numbers
         fuzzy_searches = {
             'number': blocklist_number['number'],
@@ -53,23 +49,40 @@ def test_search(user):
         }
         exact_searches = dict(
             **fuzzy_searches,
+            user_uuid=user['uuid'],
         )
 
         for field, term in fuzzy_searches.items():
-            check_search_fuzzy(url, blocklist_number, hidden, field, term)
+            response = url.get(search=term)
+            assert_that(
+                response.items, has_item(has_entries(field, blocklist_number[field]))
+            )
+            assert_that(
+                response.items, is_not(has_item(has_entries(field, hidden[field])))
+            )
 
         for field, term in exact_searches.items():
-            check_search_exact(url, blocklist_number, hidden, field, term)
+            response = url.get(**{field: term})
+            assert_that(
+                response.items, has_item(has_entries(uuid=blocklist_number['uuid']))
+            )
+            assert_that(
+                response.items, is_not(has_item(has_entries(uuid=hidden['uuid'])))
+            )
 
 
 @fixtures.user()
-def test_list(user):
+@fixtures.user()
+def test_list(user, other_user):
     user_confd_client = create_confd(user_uuid=user['uuid'])
+    other_user_confd_client = create_confd(user_uuid=other_user['uuid'])
     numbers = [
         fixtures.users_blocklist_number(confd_client=user_confd_client),
         fixtures.users_blocklist_number(confd_client=user_confd_client),
+        fixtures.users_blocklist_number(confd_client=other_user_confd_client),
+        fixtures.users_blocklist_number(confd_client=other_user_confd_client),
     ]
-    with numbers[0] as first, numbers[1] as second:
+    with utils.context_group(*numbers) as (first, second, third, fourth):
         response = confd.users.blocklist.numbers.get()
         assert_that(
             response.items,
@@ -78,11 +91,29 @@ def test_list(user):
                     uuid=first['uuid'],
                     label=first['label'],
                     number=first['number'],
+                    tenant_uuid=user['tenant_uuid'],
+                    user_uuid=user['uuid'],
                 ),
                 has_entries(
                     uuid=second['uuid'],
                     label=second['label'],
                     number=second['number'],
+                    tenant_uuid=user['tenant_uuid'],
+                    user_uuid=user['uuid'],
+                ),
+                has_entries(
+                    uuid=third['uuid'],
+                    label=third['label'],
+                    number=third['number'],
+                    tenant_uuid=other_user['tenant_uuid'],
+                    user_uuid=other_user['uuid'],
+                ),
+                has_entries(
+                    uuid=fourth['uuid'],
+                    label=fourth['label'],
+                    number=fourth['number'],
+                    tenant_uuid=other_user['tenant_uuid'],
+                    user_uuid=other_user['uuid'],
                 ),
             ),
         )
@@ -99,7 +130,7 @@ def test_sorting_offset_limit(user):
             number='+11111111112', label='sort2', confd_client=user_confd_client
         ),
     ]
-    with numbers[0] as blocklist_number1, numbers[1] as blocklist_number2:
+    with utils.context_group(*numbers) as (blocklist_number1, blocklist_number2):
         url = confd.users.blocklist.numbers.get
         list_args = (url, blocklist_number1, blocklist_number2, 'label', 'sort')
         list_kwargs = {'id_field': 'uuid'}
@@ -127,6 +158,8 @@ def test_get(user):
                 uuid=blocklist_number['uuid'],
                 label=blocklist_number['label'],
                 number=blocklist_number['number'],
+                tenant_uuid=user['tenant_uuid'],
+                user_uuid=user['uuid'],
             ),
         )
 
