38dccb60baf85c6846d22f484121608a36093fe5
Alexandre Fournier <afournier@wazo.io>
2025-11-26 09:48:49 -0500

    config_diff: add plugin

---
+++ wazo_confd/plugins/config_diff/service.py
@@ -0,0 +1,21 @@
+# Copyright 2025 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import logging
+
+from wazo_confd._sysconfd import SysconfdPublisher
+
+logger = logging.getLogger(__name__)
+
+
+class ConfigHistoryService:
+    def __init__(self, sysconfd):
+        self.sysconfd: SysconfdPublisher = sysconfd
+
+    def get_history(self):
+        config_history = self.sysconfd.get_config_history()
+        logger.debug('AFDEBUG: config_history: %s', config_history)
+        return self.sysconfd.get_config_history()
+
+    def get_diff(self, start_date: str, end_date: str):
+        return self.sysconfd.get_config_history_diff(start_date, end_date)
