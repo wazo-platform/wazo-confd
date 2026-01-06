f3238ae9c516df901d1775b1dfd57ca289628f6a
Francois Blackburn <fblackburn@wazo.community>
2026-01-06 13:09:24 -0500

    timezone: sort timezones
    why: ensure to always have same output

--- wazo_confd/plugins/timezone/resource.py
+++ wazo_confd/plugins/timezone/resource.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2025 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import zoneinfo
@@ -10,7 +10,7 @@ from wazo_confd.helpers.restful import ConfdResource
 class TimezoneList(ConfdResource):
     @required_acl('confd.timezones.get')
     def get(self):
-        timezones = zoneinfo.available_timezones()
+        timezones = sorted(zoneinfo.available_timezones())
         return {
             'total': len(timezones),
             'items': [{'zone_name': timezone} for timezone in timezones],
